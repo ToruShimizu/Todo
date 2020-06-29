3291f7a62fe3d41b555d4b401ef2a4c761b73758
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-06-30 04:01:18 +0900

    トランジションを追加

--- Todo-List/components/Todos.vue
+++ Todo-List/components/Todos.vue
@@ -1,21 +1,61 @@
 <template>
   <v-container style="max-width: 500px">
     <v-layout>
-
-    <v-icon mt-3>mdi-pen-plus</v-icon>
+    <v-flex>
+    <!-- <v-icon>mdi-pen-plus</v-icon> -->
     <v-text-field
       v-model="content"
       label="タスクを追加する"
       solo
       @keydown.enter="create"
     >
-      <v-fade-transition v-slot:append>
-        <v-icon v-if="task" @click="create">
-          add_circle
-        </v-icon>
-      </v-fade-transition>
     </v-text-field>
+      </v-flex>
+    <v-flex mt-1 ml-2>
+      <transition name="fade">
+        <v-btn v-if="contentExists"  @click="create" type="submit"  color="success">
+
+        <v-icon>
+          mdi-pen-plus
+        </v-icon>
+        </v-btn>
+      </transition>
+    </v-flex>
+  
+      <v-fade-transition v-slot:append>
+    
+      </v-fade-transition>
     </v-layout>
+
+    <h2 class="display-1 success--text pl-4">
+      Tasks:&nbsp;
+      <v-fade-transition leave-absolute>
+        <span :key="`tasks-${tasks.length}`">
+          {{ tasks.length }}
+        </span>
+      </v-fade-transition>
+    </h2>
+
+    <v-divider class="mt-4"></v-divider>
+
+    <v-row class="my-1" align="center">
+      <strong class="mx-4 info--text text--darken-2">
+        未完了: {{ remainingTasks }}
+      </strong>
+
+      <v-divider vertical></v-divider>
+
+      <strong class="mx-4 success--text text--darken-2">
+        完了: {{ completedTasks }}
+      </strong>
+
+      <v-spacer></v-spacer>
+
+      <v-progress-circular :value="progress" class="mr-2"></v-progress-circular>
+    </v-row>
+
+    <v-divider class="mb-4"></v-divider>
+
     <v-card v-if="tasks.length > 0">
       <v-slide-y-transition class="py-0" group tag="v-list">
         <template v-for="(task, i) in tasks">
@@ -62,7 +102,18 @@ export default {
   },
 
   computed: {
- 
+      contentExists () {
+      return this.content.length > 0
+    },
+    completedTasks () {
+      return this.tasks.filter(task => task.done).length
+    },
+    progress () {
+      return (this.completedTasks / this.tasks.length) * 100
+    },
+    remainingTasks () {
+      return this.tasks.length - this.completedTasks
+    },
       ...mapState([
       'tasks'
     ]),
@@ -74,8 +125,17 @@ export default {
         content: this.content,
         done: false,
       })
-      this.content = null
+      this.content = ''
     }
   }
 }
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
