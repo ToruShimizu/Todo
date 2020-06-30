a98158737d8d78ed2f81eae7bc861ea9e36536c7
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-07-01 04:34:01 +0900

    コメント追加

--- Todo-List/store/index.js
+++ Todo-List/store/index.js
@@ -4,53 +4,62 @@ export const state = () => ({
 })
 
 export const mutations = {
-  // リスト追加
+
+  // タスク追加
   create (state, payload,text) {
     state.todos.push({content: payload.content, done:false,})
   },
+  // タスク削除
   remove (state, payload) {
     state.todos.splice(state.todos.indexOf(payload.todo), 1)
   },
+  // 完了、未完了切り替え
   toggle (state, payload) {
     payload.done = !payload.done
   },
   update(state, { id, title, detail }) {
-    const index = state.todos.findIndex(todo => todo.id === id);
+    const index = state.todos.findIndex(todo => todo.id === id)
     if (index >= 0) {
-      state.todos[index].title = title;
+      state.todos[index].title = title
     }
   }
 
 }
 
 export const actions = {
-  // リスト追加
+  // タスク追加
   create (context, payload) {
     context.commit('create', payload)
   },
+  // タスク削除
   remove (context, payload) {
     context.commit('remove', payload)
   },
+  // 完了、未完了切り替え
   toggle (context, payload) {
     context.commit('toggle', payload)
   },
   updateTodo({ commit }, todo) {
-    commit("update", todo);
+    commit("update", todo)
   },
 
 }
 
 export const getters = {
+  // タスク総数のカウント
   todosCount (state) {
-    return state.todos.length;
+    return state.todos.length
   },
+  // 完了タスクのカウント
   completedTodos (state,getters) {
     return state.todos.filter(todo => todo.done).length
   },
+  // タスクの完了率
   progress (state,getters) {
     return (getters.completedTodos / state.todos.length) * 100
   },
+  // 未完了タスクのカウント
   remainingTodos (state,getters) {
     return state.todos.length - getters.completedTodos
-  },
+  }
 }
