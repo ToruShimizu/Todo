4d20b76331929903f212cc8782ebcba2f7ea7e57
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-07-04 02:01:29 +0900

    ログイン機能の実装

--- Todo-List/layouts/default.vue
+++ Todo-List/layouts/default.vue
@@ -1,6 +1,20 @@
 <template>
-  <v-app dark>
-
+  <v-app dark> 
+    <v-card
+    color="grey lighten-4"
+    flat
+    tile
+  >
+      <v-toolbar color="indigo darken-2" dense dark >
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <v-toolbar-title>
+        ToDo
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon @click="logout">mdi-logout-variant</v-icon>
+      <v-icon @click="login">mdi-account</v-icon>
+    </v-toolbar>
+    </v-card>
       <todos/>
         <nuxt />
       
@@ -22,8 +36,36 @@ export default {
       
     }
   },
+   created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setLoginUser(user)
+      }else {
+        this.deleteLoginUser()
+      }
+  })
+  },
+    async asyncData ({ redirect, store }) {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        const { uid, displayName, email } = user
+        store.dispatch ('setUser', { user: { id: uid, name: displayName, email: email }})
+
+        const cookies = new Cookies()
+        const cookie = {id: uid, name: displayName, email: email}
+        cookies.set('user', JSON.stringify(cookie))
+        redirect('/todos')
+      }
+    })
+    return {
+    }
+  },
+  
+ 
+
   methods: {
-    ...mapActions(['login','setLoginUser'])
+    
+    ...mapActions(['login','setLoginUser','logout'])
   }
 }
 </script>
