17818d4398bb15d3a23396a11c89e3b3c271fc88
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-07-03 17:11:19 +0900

    ログアウト機能の実装

--- Todo-List/pages/index.vue
+++ Todo-List/pages/index.vue
@@ -2,10 +2,12 @@
      <v-container text-center justify-center>
     <v-layout row wrap>
       <v-flex xs12>
-        <h1>マイアドレス帳</h1>
-        <p>マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。</p>
+        <h1></h1>
+        <p>Googleアカウントでログインしてください。</p>
       </v-flex>
-
+    <v-toolbar-items>
+        <v-btn text @click="logout">ログアウト</v-btn>
+      </v-toolbar-items>
       <v-flex xs12 mt-5>
         <v-btn color='info' @click="login">Googleアカウントでログイン</v-btn>
       </v-flex>
@@ -26,11 +28,13 @@ export default {
     firebase.auth().onAuthStateChanged(user => {
       if(user) {
         this.setLoginUser(user)
+      }else {
+        this.deleteLoginUser()
       }
   })
   },
     methods: {
-    ...mapActions(['login','setLoginUser'])
+    ...mapActions(['login','setLoginUser','deleteLoginUser','logout'])
   }
 }
 </script>
