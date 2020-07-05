77bfc9c9c068ba337deea2e57b44e19e7749c310
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-07-05 10:40:43 +0900

    ログイン状態に応じた表示の切り替え

--- Todo-List/layouts/default.vue
+++ Todo-List/layouts/default.vue
@@ -1,71 +1,69 @@
 <template>
-  <v-app dark> 
-    <v-card
-    color="grey lighten-4"
-    flat
-    tile
-  >
-      <v-toolbar color="indigo darken-2" dense dark >
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-      <v-toolbar-title>
-        ToDo
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-icon @click="logout">mdi-logout-variant</v-icon>
-      <v-icon @click="login">mdi-account</v-icon>
-    </v-toolbar>
+  <v-app dark>
+    <v-card color="grey lighten-4" flat tile>
+      <v-toolbar color="indigo darken-2" dense dark>
+        <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <v-toolbar-side-icon v-show="$store.state.login_user" @click.stop="toggleSideMenu">
+      </v-toolbar-side-icon>
+
+        <v-toolbar-title>
+          ToDo
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-icon v-if="$store.state.login_user" @click="logout"
+          >mdi-logout-variant</v-icon
+        >
+        <v-icon v-if="!$store.state.login_user" @click="login"
+          >mdi-account</v-icon
+        >
+      </v-toolbar>
     </v-card>
-      <todos/>
-        <nuxt />
-      
+    <todos />
+    <nuxt />
   </v-app>
 </template>
 
 <script>
-import Todos from '@/components/Todos'
-import {mapActions} from 'vuex'
-import firebase from 'firebase'
+import Todos from '@/components/Todos';
+import { mapActions } from 'vuex';
+import firebase from 'firebase';
 
 export default {
   components: {
     Todos
   },
- 
-  data () {
-    return {
-      
-    }
+
+  data() {
+    return {}
   },
-   created () {
+  created() {
     firebase.auth().onAuthStateChanged(user => {
-      if(user) {
+      if (user) {
         this.setLoginUser(user)
-      }else {
+      } else {
         this.deleteLoginUser()
       }
-  })
+    })
   },
-    async asyncData ({ redirect, store }) {
+  async asyncData({ redirect, store }) {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         const { uid, displayName, email } = user
-        store.dispatch ('setUser', { user: { id: uid, name: displayName, email: email }})
+        store.dispatch('setUser', {
+          user: { id: uid, name: displayName, email: email }
+        })
 
         const cookies = new Cookies()
-        const cookie = {id: uid, name: displayName, email: email}
+        const cookie = { id: uid, name: displayName, email: email }
         cookies.set('user', JSON.stringify(cookie))
-        redirect('/todos')
+        redirect('/todos');
       }
     })
-    return {
-    }
+    return {}
   },
-  
- 
 
   methods: {
-    
-    ...mapActions(['login','setLoginUser','logout'])
+    ...mapActions(['login', 'setLoginUser', 'logout', 'deleteLoginUser','toggleSideMenu'])
   }
-}
+};
 </script>
