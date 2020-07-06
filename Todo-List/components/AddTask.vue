fd0b1760177925c46d9038764eae845d77d2d793
ToruShimizu <nobbyz.t.nn@gmail.com>
2020-07-06 16:55:29 +0900

    コンポーネント作成

---
+++ Todo-List/components/AddTask.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">AddToTask</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar-today"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+              <v-menu
+        ref="menu"
+        v-model="menu2"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        :return-value.sync="time"
+        transition="scale-transition"
+        offset-y
+        max-width="290px"
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="time"
+            label="Picker in menu"
+            prepend-icon="mdi-clock-time-four-outline"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-time-picker
+          v-if="menu2"
+          v-model="time"
+          full-width
+          @click:minute="$refs.menu.save(time)"
+        ></v-time-picker>
+      </v-menu>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+
+            <v-text-field
+
+          v-model="task"
+          label="タスクを追加する"
+          prepend-inner-icon="mdi-pencil-plus-outline"
+          persistent-hint
+          @keydown.enter="addTask"
+        />
+
+
+
+
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="詳細を追加する" prepend-inner-icon="mdi-briefcase-outline" required></v-text-field>
+              </v-col>
+
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @keydown.enter="addTask">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      task: '',
+      date: new Date().toISOString().substr(0, 10),
+      dialog: false,
+       time: null,
+        menu: false,
+        menu2: false,
+    }),
+  }
+</script>
+
---
+++ Todo-List/components/Board.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app grey>
+   <todos />
+  </v-app>
+</template>
+
+<script>
+import Todos from '../components/Todos'
+export default {
+  components: {
+    Todos
+  }
+}
+</script>
