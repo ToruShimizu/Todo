e49ada3d04e3169bd38f0fb9e6d533224d17fbf5
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-07-13 17:54:20 +0900

    ユーザー画像と名前を表示する

--- Todo-List/layouts/default.vue
+++ Todo-List/layouts/default.vue
@@ -2,25 +2,22 @@
   <v-app dark>
     <v-card color="grey lighten-4" flat tile>
       <v-toolbar color="indigo darken-2" dense dark>
-      <!-- <v-toolbar-side-icon v-show="$store.state.login_user" @click.stop="toggleSideMenu"> -->
-      <!-- </v-toolbar-side-icon> -->
-
-        <v-toolbar-title>
-          ToDo
-        </v-toolbar-title>
+        <v-toolbar-title>ToDo</v-toolbar-title>
         <v-spacer></v-spacer>
-              <v-toolbar-items v-if="$store.state.login_user">
 
-        <v-icon @click="logout"
-          >mdi-logout-variant</v-icon
-        >
-              </v-toolbar-items>
-                    <v-toolbar-items v-if="!$store.state.login_user">
-
-        <v-icon @click="login"
-          >mdi-account</v-icon
-        >
-                    </v-toolbar-items>
+        <v-toolbar-items v-if="$store.state.login_user">
+          <v-toolbar-title>{{userName}}</v-toolbar-title>
+          <v-avatar size="40">
+      <img
+      v-if="photoURL"
+        :src="photoURL"
+      >
+    </v-avatar>
+          <v-icon @click="logout">mdi-logout-variant</v-icon>
+        </v-toolbar-items>
+        <v-toolbar-items v-if="!$store.state.login_user">
+          <v-icon @click="login">mdi-account</v-icon>
+        </v-toolbar-items>
       </v-toolbar>
     </v-card>
     <todos />
@@ -29,46 +26,49 @@
 </template>
 
 <script>
-import Todos from '@/components/Todos';
-import { mapActions } from 'vuex';
-import firebase from 'firebase';
+import Todos from "@/components/Todos";
+import { mapActions, mapGetters } from "vuex";
+import firebase from "firebase";
 
 export default {
   components: {
-    Todos,
+    Todos
   },
 
   data() {
-    return {}
+    return {};
   },
   created() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.setLoginUser(user)
+        this.setLoginUser(user);
       } else {
-        this.deleteLoginUser()
+        this.deleteLoginUser();
       }
-    })
+    });
   },
   async asyncData({ redirect, store }) {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        const { uid, displayName, email } = user
-        store.dispatch('setUser', {
+        const { uid, displayName, email } = user;
+        store.dispatch("setUser", {
           user: { id: uid, name: displayName, email: email }
-        })
+        });
 
-        const cookies = new Cookies()
-        const cookie = { id: uid, name: displayName, email: email }
-        cookies.set('user', JSON.stringify(cookie))
-        redirect('/todos');
+        const cookies = new Cookies();
+        const cookie = { id: uid, name: displayName, email: email };
+        cookies.set("user", JSON.stringify(cookie));
+        redirect("/todos");
       }
-    })
-    return {}
+    });
+    return {};
+  },
+  computed: {
+    ...mapGetters(["userName", "photoURL"])
   },
 
   methods: {
-    ...mapActions(['login', 'setLoginUser', 'logout', 'deleteLoginUser','toggleSideMenu'])
+    ...mapActions(["login", "setLoginUser", "logout", "deleteLoginUser"])
   }
 };
 </script>
