2bc3d8722df8b168ad6ed7e2244282223157cf83
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-07-13 12:47:43 +0900

    編集機能を作成

--- Todo-List/store/index.js
+++ Todo-List/store/index.js
@@ -4,127 +4,151 @@ import { db } from '~/plugins/firebase'
 export const strict = false
 
 export const state = () => ({
-  todos: [{task:'プログラミング学習',detail:'Nuxt.jsとfirebaseでTodoリストを作る',date:'2020-07-01',time:'19:00',done:false}],
+  todos: [
+    {
+      task: 'プログラミング学習',
+      detail: 'Nuxt.jsとfirebaseでTodoリストを作る',
+      date: '2020-07-01',
+      time: '19:00',
+      done: false
+    }
+  ],
   login_user: null,
-  drawer: false,
+  drawer: false
 })
 
 export const mutations = {
   // ログインユーザー情報の取得
-  setLoginUser (state, user) {
+  setLoginUser(state, user) {
     state.login_user = user
   },
   // ログインユーザー情報の削除
   deleteLoginUser(state) {
     state.login_user = null
   },
-  toggleSideMenu (state) {
+  toggleSideMenu(state) {
     state.drawer = !state.drawer
   },
   // タスク追加
-  addTask (state, { todo }) {
-    state.todos.push({ date:todo.date,time:todo.time,task: todo.task, detail:todo.detail,done: false })
+  addTask(state, { todo }) {
+    state.todos.push({
+      date: todo.date,
+      time: todo.time,
+      task: todo.task,
+      detail: todo.detail,
+      done: false
+    })
   },
   // タスク削除
-  removeTask (state, { todo }) {
+  removeTask(state, { todo }) {
     state.todos.splice(state.todos.indexOf(todo.task), 1)
   },
-  // 完了、未完了切り替え
-  doneTask (state, { todo }) {
-    todo.done = !todo.done
+  updateTask(state, todo) {
+    todo.task = todo.editTask
+    todo.detail = todo.editDetail
+    todo.date = todo.editDate
+    todo.time = todo.editTime
   },
-
+  // 完了、未完了切り替え
+  doneTask(state, { todo }) {
+    todo.done = !todo.done
+  }
 }
 
 export const actions = {
   // ログインユーザー情報の取得
-  setLoginUser ( { commit }, user) {
+  setLoginUser({ commit }, user) {
     commit('setLoginUser', user)
   },
   // ログインユーザー情報の削除
-  deleteLoginUser({ commit }){
+  deleteLoginUser({ commit }) {
     commit('deleteLoginUser')
   },
-  toggleSideMenu ({ commit }) {
+  toggleSideMenu({ commit }) {
     commit('toggleSideMenu')
   },
   // ログイン
-  login () {
+  login() {
     const provider = new firebase.auth.GoogleAuthProvider()
-    auth.signInWithPopup(provider)
-    .then((result) => {
-      alert('Hello, '+result.user.displayName+'!')
+    auth.signInWithPopup(provider).then(result => {
+      alert('Hello, ' + result.user.displayName + '!')
     })
   },
   // ログアウト
-  logout () {
+  logout() {
     alert('ログアウトしました')
     auth.signOut()
   },
   // タスク追加
-  addTask ({ commit }, todo) {
-
-      db.collection('users').doc('user1').set({task:todo.task}).then(function() {
-
-        console.log("Document successfully written!")
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-    commit("addTask", { todo })
+  addTask({ commit }, todo) {
+    db.collection('users')
+      .doc('user1')
+      .set({
+        task: todo.task,
+        detail: todo.detail,
+        date: todo.date,
+        time: todo.time
+      })
+      .then(function() {
+        console.log('Document successfully written!')
+      })
+      .catch(function(error) {
+        console.error('Error writing document: ', error)
+      })
+    commit('addTask', { todo })
   },
   // タスク削除
-  removeTask ({ commit }, todo) {
-    db.collection("users").doc("user1").delete().then(function() {
-      console.log("Document successfully deleted!");
-  }).catch(function(error) {
-      console.error("Error removing document: ", error)
-  })
-  commit("removeTask", { todo })
+  removeTask({ commit }, todo) {
+    db.collection('users')
+      .doc('user1')
+      .delete()
+      .then(function() {
+        console.log('Document successfully deleted!')
+      })
+      .catch(function(error) {
+        console.error('Error removing document: ', error)
+      })
+    commit('removeTask', { todo })
   },
   // 完了、未完了切り替え
-  doneTask ({ commit }, todo) {
-    commit("doneTask", { todo })
+  doneTask({ commit }, todo) {
+    commit('doneTask', { todo })
   },
-  updateTask ({ commit },todo) {
-    const taskRef = db.collection("users").doc("user1")
-    commit("updateTask",{ todo })
+  updateTask({ commit }, todo) {
+    const taskRef = db.collection('user').doc('user1')
 
-    // Set the "capital" field of the city 'DC'
-    // taskRef.update({
-    //    task:todo.editTask,
-    //    detail: payload.editDetail,
-    //    date: payload.editDate,
-    //    time: payload.editTime
-    // })
-    // .then(function() {
-    //     console.log("Document successfully updated!");
-    // })
-    // .catch(function(error) {
-    //     // The document probably doesn't exist.
-    //     console.error("Error updating document: ", error);
-    // })
-},
+    // Set the 'capital' field of the city 'DC'
+    taskRef
+      .update({})
+      .then(function() {
+        console.log('Document successfully updated!')
+      })
+      .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error('Error updating document: ', error)
+      })
+    commit('updateTask', todo)
+  }
 }
 
 export const getters = {
-  userName: state => state.login_user ? state.login_user.displayName : '',
-  photoURL: state => state.login_user ? state.login_user.photoURL: '',
-  uid: state => state.login_user ? state.login_user.uid: null,
+  userName: state => (state.login_user ? state.login_user.displayName : ''),
+  photoURL: state => (state.login_user ? state.login_user.photoURL : ''),
+  uid: state => (state.login_user ? state.login_user.uid : null),
   // タスク総数のカウント
-  todosCount (state) {
+  todosCount(state) {
     return state.todos.length
   },
   // 完了タスクのカウント
-  completedTodos (state) {
+  completedTodos(state) {
     return state.todos.filter(todo => todo.done).length
   },
   // タスクの完了率
-  progress (state, getters) {
+  progress(state, getters) {
     return (getters.completedTodos / state.todos.length) * 100
   },
   // 未完了タスクのカウント
-  remainingTodos (state, getters) {
+  remainingTodos(state, getters) {
     return state.todos.length - getters.completedTodos
   }
 }
