a517543f049b6e07184c0c0ed8f778310244da21
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-06 19:11:49 +0900

    Remove: 不要なコードを削除

--- Todo-List/components/Todos.vue
+++ Todo-List/components/Todos.vue
@@ -18,65 +18,61 @@
     <v-divider class="mt-4" />
 
     <v-card v-if="todos.length > 0">
-        <!-- 完了、未完了のタブ切り替え -->
-        <v-tabs>
-          <v-tab @click="taskFilter = 'all'">すべて:{{ todos.length }}</v-tab>
+      <!-- 完了、未完了のタブ切り替え -->
+      <v-tabs>
+        <v-tab @click="taskFilter = 'all'">すべて:{{ todos.length }}</v-tab>
 
-          <v-divider vertical />
+        <v-divider vertical />
 
-          <v-tab @click="taskFilter = 'active'">未完了:{{ remainingTodos }}</v-tab>
+        <v-tab @click="taskFilter = 'active'">未完了:{{ remainingTodos }}</v-tab>
 
-          <v-divider vertical />
+        <v-divider vertical />
 
-          <v-tab @click="taskFilter = 'done'">
-            完了: {{ completedTodos }}/{{todos.length}}
-            <!-- 完了率の表示 -->
-            <v-progress-circular
-              :value="progress"
-              :rotate="270"
-              :size="45"
-              class="ml-1"
-              color="success"
-            >{{progress}}</v-progress-circular>
-          </v-tab>
-        </v-tabs>
-                  <v-divider  />
-
-            <v-divider  />
-
+        <v-tab @click="taskFilter = 'done'">
+          完了: {{ completedTodos }}/{{todos.length}}
+          <!-- 完了率の表示 -->
+          <v-progress-circular
+            :value="progress"
+            :rotate="270"
+            :size="45"
+            class="ml-1"
+            color="success"
+          >{{progress}}</v-progress-circular>
+        </v-tab>
+      </v-tabs>
+      <v-divider />
+      <v-divider />
       <v-list>
-            <v-list-item v-for="todo in todosFiltered" :key="todo.id">
-              <!-- 完了、未完了切り替えチェックボックス -->
-              <v-btn icon>
-                <v-icon
-                  :color="(!todo.done && 'grey') || 'primary'"
-                  @click="doneTask(todo)"
-                >mdi-check-circle-outline</v-icon>
-              </v-btn>
-              <v-list-item-content>
-                <v-list-item-title
-                  @click="openTask(todo)"
-                  :class="(todo.done && 'grey--text') || 'primary--text'"
-                  class="ml-2"
-                >{{ todo.title }}</v-list-item-title>
-                <!-- 編集用のテキストエリア -->
-              </v-list-item-content>
-                 <nuxt-link :to="{ name: 'todos-edit-id', params: { id: todo.id } }">
-          Edit
-        </nuxt-link>
-              <!-- 削除ボタン -->
-              <v-btn icon>
-                <v-icon @click="removeTask(todo.id)">mdi-delete-outline</v-icon>
-              </v-btn>
-            </v-list-item>
+        <v-list-item v-for="todo in todosFiltered" :key="todo.id">
+          <!-- 完了、未完了切り替えチェックボックス -->
+          <v-btn icon>
+            <v-icon
+              :color="(!todo.done && 'grey') || 'primary'"
+              @click="doneTask(todo)"
+            >mdi-check-circle-outline</v-icon>
+          </v-btn>
+          <v-list-item-content>
+            <v-list-item-title
+              @click="openTask(todo)"
+              :class="(todo.done && 'grey--text') || 'primary--text'"
+              class="ml-2"
+            >{{ todo.title }}</v-list-item-title>
+            <!-- 編集用のテキストエリア -->
+          </v-list-item-content>
+          <nuxt-link :to="{ name: 'todos-edit-id', params: { id: todo.id } }">Edit</nuxt-link>
+          <!-- 削除ボタン -->
+          <v-btn icon>
+            <v-icon @click="removeTask(todo.id)">mdi-delete-outline</v-icon>
+          </v-btn>
+        </v-list-item>
       </v-list>
     </v-card>
   </v-container>
 </template>
 
 <script>
-import { mapState, mapGetters, } from "vuex";
-import AddTask from "@/components/AddTask";
+import { mapState, mapGetters } from 'vuex'
+import AddTask from '@/components/AddTask'
 
 export default {
   components: {
@@ -84,79 +80,69 @@ export default {
   },
   data() {
     return {
-      date: new Date().toISOString().substr(0, 10),
-      time: '',
       taskFilter: 'all',
       searchTask: '',
-      editTitle: '',
-      editDetail: '',
-      editDate: '',
-      editTime: '',
-      detailTask: false,
-      selectDate: false,
-      selectTime: false
-    };
+    }
   },
-    created() {
+  created() {
     this.$store.dispatch('fetchTodos')
   },
   computed: {
     todosFiltered() {
       // タスク検索
-      let arr = [];
-      let data = this.todos;
+      let arr = []
+      let data = this.todos
       if (this.searchTask) {
-        data.forEach(el => {
+        data.forEach((el) => {
           if (
             el.task.toLowerCase().indexOf(this.searchTask.toLowerCase()) >= 0
           ) {
-            arr.push(el);
+            arr.push(el)
           }
-        });
-        return arr;
+        })
+        return arr
       }
       // 完了状態の絞り込み
-      else if (this.taskFilter == "all") {
-        return this.todos;
-      } else if (this.taskFilter == "active") {
-        return this.todos.filter(todo => !todo.done);
-      } else if (this.taskfilter == "done")
-        return this.todos.filter(todo => todo.done);
+      else if (this.taskFilter == 'all') {
+        return this.todos
+      } else if (this.taskFilter == 'active') {
+        return this.todos.filter((todo) => !todo.done)
+      } else if (this.taskfilter == 'done')
+        return this.todos.filter((todo) => todo.done)
     },
     ...mapGetters([
-      "completedTodos",
-      "progress",
-      "remainingTodos",
-      "todosCount",
+      'completedTodos',
+      'progress',
+      'remainingTodos',
+      'todosCount',
     ]),
-    ...mapState(["todos"])
+    ...mapState(['todos']),
   },
 
   methods: {
-     removeTask(id) {
+    removeTask(id) {
       if (!confirm('Are you sure?')) return
 
-      this.$store.dispatch('removeTask', { id })
-      .then(() => {
+      this.$store.dispatch('removeTask', { id }).then(() => {
         setTimeout(() => {
           this.$store.dispatch('fetchTodos')
         }, 1000)
       })
-  },
+    },
 
     doneTask(todo) {
-      this.$store.dispatch("doneTask", todo);
+      this.$store.dispatch('doneTask', todo)
     },
-  }
-};
+  },
+}
 </script>
 
 <style>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity 0.5s
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
+  opacity: 0
 }
 </style>
