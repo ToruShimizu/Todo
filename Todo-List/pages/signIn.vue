4bc4276026f124a931bfa75664f3629405973b81
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-09 06:20:01 +0900

    #38 Add: ログイン画面の雛形を作成

---
+++ Todo-List/pages/signIn.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1>Todo</h1>
+        <p>
+          ご利用の方は、
+          <br />こちらからログインしてください。
+        </p>
+      </v-card-title>
+      <v-card-text>
+        <v-btn
+          class="fill-width text-capitalize caption"
+          height="48px"
+          outlined
+          style="border-color:#979797;"
+          tile
+        >
+          <img
+            class="button-logo-img mr-4"
+            src="https://madeby.google.com/static/images/google_g_logo.svg"
+            style="height:24px;"
+          />Googleアカウントでログイン
+        </v-btn>
+        <v-btn
+          class="fill-width text-capitalize caption mt-5"
+          height="48px"
+          outlined
+          style="border-color:#979797;"
+          tile
+        >アカウントなしででログイン</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
