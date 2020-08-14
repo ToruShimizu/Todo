309919b75c99132ea606388719393c70c25c63c3
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-15 07:36:25 +0900

    Add: タスク追加と編集を同じページで使えるように実装

--- Todo-List/pages/edit/_id.vue
+++ Todo-List/pages/edit/_id.vue
@@ -1,106 +1,157 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="detailTask" persistent max-width="600px">
+    <v-dialog v-model="taskDialog" persistent max-width="600px">
       <v-card>
         <v-card-title>
-          <span class="headline">detailTask</span>
+          <span class="headline">AddToTask</span>
         </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-               <!-- タスク変更エリア -->
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  :value="$store.getters.getTask.title"
-                  label="タスクを変更する"
-                  prepend-inner-icon="mdi-pencil-outline"
-                  clearable
-                />
-              </v-col>
-                <!-- 日付変更エリア -->
-              <v-col cols="12" sm="6" md="6">
-                <v-dialog
-                ref="dialog"
-                v-model="modal"
-                :return-value.sync="$store.getters.getTask.date"
-                persistent
-                width="290px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      :value="$store.getters.getTask.date"
-                      label="Picker in dialog"
-                      prepend-inner-icon="mdi-calendar-today"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker :value="$store.getters.getTask.date" scrollable range>
-                    <v-spacer></v-spacer>
-                    <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="$refs.dialog.save($store.getters.getTask.date)"
-                    >OK</v-btn>
-                  </v-date-picker>
-                </v-dialog>
-              </v-col>
-
-                <!-- 詳細変更エリア -->
-              <v-col cols="12">
-                <v-text-field
-                  :value="$store.getters.getTask.detail"
-                  label="詳細を変更する"
-                  prepend-inner-icon="mdi-briefcase-outline"
-                  required
-                  clearable
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <nuxt-link to="/">
-            <v-btn color="blue darken-1" text>Close</v-btn>
-          </nuxt-link>
-          <v-btn color="blue darken-1" text @click="editTask">Save</v-btn>
-        </v-card-actions>
+        <v-form ref="form"
+    lazy-validation >
+          <v-card-text>
+            <v-container>
+              <v-row>
+                     <!-- タスク入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="task.title"
+                    label="タスクを追加する"
+                    prepend-inner-icon="mdi-pencil-outline"
+                    @keydown.enter="addTask"
+                    :rules="titleRules"
+                    clearable
+                  />
+                </v-col>
+                  <!-- 日付入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-dialog
+                    ref="dialog"
+                    v-model="modal"
+                    :return-value.sync="task.date"
+                    persistent
+                    width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="dateRangeText"
+                        label="Picker in dialog"
+                        prepend-inner-icon="mdi-calendar-today"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="task.date" scrollable range>
+                      <v-spacer></v-spacer>
+                      <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
+                      <v-btn text color="primary" @click="$refs.dialog.save(task.date)">OK</v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+                </v-col>
+                  <!-- 詳細入力エリア -->
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="task.detail"
+                    label="詳細を追加する"
+                    prepend-inner-icon="mdi-briefcase-outline"
+                    required
+                    clearable
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <nuxt-link to='/'>
+            <v-btn color="blue darken-1" text @click="closeTaskDialog">Close</v-btn>
+            </nuxt-link>
+            <v-btn color="blue darken-1" text @click="addTask">Save</v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
     </v-dialog>
   </v-row>
 </template>
 
 <script>
+import { mapActions} from 'vuex'
 export default {
-  created() {
-    const id = this.$route.params.id
-    this.$store.dispatch('fetchTask', { id })
+    created () {
+    // ルートのパラメーターにアドレスIdが含まれているか
+    if (!this.$route.params.address_id) return
+    // 引数にアドレスのIdを渡すことで該当のアドレスを取得
+    const task = this.$store.getters.getAddressById(this.$route.params.task_id)
+    if (task) {
+      // 取得できれば格納
+      this.task = task
+    } else {
+      // できなければ一覧ページへ遷移
+      this.$router.push({ path: '/' })
+    }
   },
   data() {
     return {
-      detailTask: true,
+      task: {
+        title: '',
+        detail: '',
+        date: [new Date().toISOString().substr(0, 10)],
+        done: false,
+      },
+      validate: true,
+      titleRules:[
+        v => !!v || 'タイトルは必須入力です'
+        ],
       modal: false,
-      selectDate2: false,
+      taskDialog: true,
+      menu: false,
     }
   },
-
+  computed: {
+    dateRangeText () {
+      const date = this.task.date
+      return Object.values(date).join('~')
+      },
+    },
   methods: {
-    editTask(e) {
-      const task = {
-        id: this.$store.getters.getTask.id,
-        title: e.target.title.value,
-        // detail: e.target.detail.value,
-        // date: e.target.date.value,
+    addTask() {
+      if (!this.task.title) {
+        this.$refs.form.validate()
+        return
       }
-      this.$store.dispatch('editTask', { task }).then(() => {
+          if (this.$route.params.task_id) {
+        this.updateTask({ id: this.$route.params.task_id, task: this.task })
+      } else {
+        this.$store.dispatch('addTask',{task:this.task}).then(() => {
         setTimeout(() => {
-          this.$router.push('/')
+          this.$store.dispatch('fetchTodos')
         }, 1000)
       })
+      }
+      this.$router.push({ path: '/' })
+      this.task.title = ''
+      this.task.detail = ''
+      this.task.date = [new Date().toISOString().substr(0, 10)]
     },
+      //   this.$store.dispatch('addTask',{task:this.task}).then(() => {
+      //   setTimeout(() => {
+      //     this.$store.dispatch('fetchTodos')
+      //   }, 1000)
+      // this.task.title = ''
+      // this.task.detail = ''
+      // this.task.date = [new Date().toISOString().substr(0, 10)]
+      // this.taskDialog = false
+      // })
+
+    // },
+    closeTaskDialog () {
+      this.task.title = ''
+      this.task.detail = ''
+      this.task.date = [new Date().toISOString().substr(0, 10)]
+      this.taskDialog = false
+    }
   },
+      ...mapActions(['addTask', 'updateTask'])
+
 }
 </script>
+
