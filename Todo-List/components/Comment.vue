f03c29ff9461f286a9d7fe38c638adb3e12eef39
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-23 15:23:44 +0900

    #25 Add:　コンポーネントを作成し、コメント画面の雛形を作成

---
+++ Todo-List/components/Comment.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card max-width="400" class="mx-auto">
+    <v-container>
+      <v-layout>
+        <f-flex>
+          <v-text-field
+            flat
+            solo-inverted
+            hide-details
+            v-model="message"
+            prepend-inner-icon="mdi-message-text-outline"
+            label="Add Comment"
+            clearable
+          ></v-text-field>
+        </f-flex>
+        <v-flex>
+          <v-btn @click="addComment" color="primary">
+            <v-icon left>mdi-pen-plus</v-icon>Add
+          </v-btn>
+        </v-flex>
+      </v-layout>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>{{message}}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hoge",
+    };
+  },
+};
+</script>
