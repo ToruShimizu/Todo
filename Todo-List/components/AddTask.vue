3464f59cb7d5f32ace6e1b861f2afc83f31c4316
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-25 18:45:00 +0900

    #50 Add: タスクを追加するコンポーネントを作成

---
+++ Todo-List/components/AddTask.vue
@@ -0,0 +1,58 @@
+<template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">AddToTask</span>
+        </v-card-title>
+        <v-form ref="form" lazy-validation>
+            <v-container>
+              <v-row>
+                <!-- タスク入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field
+                    v-model="task.title"
+                    label="Add Task"
+                    prepend-inner-icon="mdi-pencil-outline"
+                    @keydown.enter="addTask"
+                    :rules="titleRules"
+                    clearable
+                  />
+                </v-col>
+                <!-- 日付入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-dialog
+                    ref="dialog"
+                    v-model="modal"
+                    :return-value.sync="task.date"
+                    persistent
+                    width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="task.date"
+                        label="Picker in dialog"
+                        prepend-inner-icon="mdi-calendar-today"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="task.date" scrollable range>
+                      <v-spacer></v-spacer>
+                      <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
+                      <v-btn text color="primary" @click="$refs.dialog.save(task.date)">OK</v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+                </v-col>
+              </v-row>
+            </v-container>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <nuxt-link to="/">
+              <v-btn color="blue darken-1" text>Cancel</v-btn>
+            </nuxt-link>
+            <v-btn color="blue darken-1" text @click="saveTask">Save</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+</template>
+
