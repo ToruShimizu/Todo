f1bc387ac1866e104c3b56e10e3ac7ba675f193f
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-29 20:24:49 +0900

    Fix: カードの表示を変更

--- Todo-List/components/Comment.vue
+++ Todo-List/components/Comment.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-card max-width="400" class="mx-auto">
     <v-container>
       <v-row dense>
         <v-col cols="12" v-for="(message,i) in comments" :key="i">
-          <v-card>
+          <v-card max-width="400" class="mx-auto">
             <v-card-title>{{message}}</v-card-title>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
-  </v-card>
 </template>
 
 <script>
