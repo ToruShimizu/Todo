8c0fb2d515d525c775ef9fa21fda26a04335d5dd
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-31 23:06:31 +0900

    #50 Add: HeaderコンポーネントとFooterをDefaultで表示させる

--- Todo-List/layouts/default.vue
+++ Todo-List/layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <v-app dark>
+    <Header />
     <nuxt />
+    <v-footer color="indigo darken-2" dark app>
+      <span class="grey--text text--lighten-2">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
