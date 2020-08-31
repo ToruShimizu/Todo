8d4a60ad8a4ca6c561d819e16469e334e345e923
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-08-31 23:05:34 +0900

    #50 Add: コンポーネント化に伴い不要なコードを削除

--- Todo-List/pages/index.vue
+++ Todo-List/pages/index.vue
@@ -1,68 +1,19 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-container>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title grey--text text--darken-2">Login User</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider></v-divider>
-        <v-list dense nav>
-          <v-list-item v-if="$store.state.login_user">
-            <v-list-item-icon v-if="photoURL">
-              <v-avatar size="50">
-                <img :src="photoURL" />
-              </v-avatar>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="title grey--text text--darken-2">{{userName}}</v-list-item-title>
-              <v-list-item-title class="title grey--text text--darken-2" v-if='!userName'>{{userEmail}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item v-else>
-            <v-list-item-content>
-              <v-list-item-title>Not Login</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-container>
-    </v-navigation-drawer>
-    <v-app-bar color="indigo darken-2" dark app clipped-left>
-      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>ToDo</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items v-if="$store.state.login_user">
-        <v-icon @click="logout">mdi-logout-variant</v-icon>
-      </v-toolbar-items>
-      <v-toolbar-items v-if="!$store.state.login_user">
-        <v-icon @click="login">mdi-account</v-icon>
-      </v-toolbar-items>
-    </v-app-bar>
     <v-container mt-12>
       <todos />
     </v-container>
-    <v-footer color="indigo darken-2" dark app>
-      <span class="grey--text text--lighten-2">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 import Todos from "@/components/Todos";
-import { mapActions, mapGetters } from "vuex";
+import { mapActions } from "vuex";
 import firebase from "firebase";
 
 export default {
   components: {
     Todos,
-  },
-
-  data() {
-    return {
-      drawer: null,
-    };
   },
   created() {
     firebase.auth().onAuthStateChanged((user) => {
@@ -76,15 +27,9 @@ export default {
       }
     });
   },
-  computed: {
-    ...mapGetters(["userName", "photoURL","userEmail"]),
-  },
-
   methods: {
     ...mapActions([
-      "login",
       "setLoginUser",
-      "logout",
       "deleteLoginUser",
       "fetchTodos",
     ]),
