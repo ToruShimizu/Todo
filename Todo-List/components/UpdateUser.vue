0fb0adb3580ea1690bbb245ad023f6af48fc63cd
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-09-07 00:45:02 +0900

    #67 Add: ユーザー情報を変更するコンポーネントを作成

---
+++ Todo-List/components/UpdateUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app class="mt-12">
+    <v-col cols="12" sm="12" md="12">
+      <v-card width="400px" class="mx-auto mt-5 text-center">
+        <v-card-title>
+          <h2>ユーザー情報編集</h2>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" lazy-validation @submit.prevent="createUser">
+            <v-text-field
+              prepend-inner-icon="mdi-card-account-details-outline"
+              label="Name"
+              v-model="updateUserName"
+              :rules="nameRules"
+              clearable
+            />
+            <v-card-actions>
+              <v-btn @click="updateUser" color="success">
+                <v-icon left>mdi-account</v-icon>SAVE
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="$emit('close-edit-user')">
+                <v-icon left>mdi-login-variant</v-icon>戻る
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      updateUserName: "",
+      validate: true,
+      nameRules: [(v) => !!v || "名前は必須です"],
+    };
+  },
+  methods: {
+    updateUser() {
+      this.$store.dispatch("updateUser", {
+        userName: this.updateUserName,
+      });
+    },
+  },
+};
+</script>
+
