3fa2d8bef75ea3c63ad789d084f47bbb39957835
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-09-27 08:43:04 +0900

    #89 Fix: ループ処理が発生してしまうため、条件分岐にて処理を追加。

--- Todo-List/middleware/authenticated.js
+++ Todo-List/middleware/authenticated.js
@@ -1,21 +1,23 @@
-import { auth } from "../plugins/firebase";
+import { auth } from '../plugins/firebase'
 
-export default function({ store, redirect }) {
-  if(store.state["modules"].auth.login_user) return
- auth.onAuthStateChanged((user) => {
+export default function ({ store, redirect, route }) {
+  if (store.state.modules.auth.login_user) return
+  auth.onAuthStateChanged((user) => {
     if (user) {
       const userInfo = {
         displayName: user.displayName,
         email: user.email,
         photoURL: user.photoURL,
-        uid: user.uid
-      };
-      store.dispatch("modules/auth/setLoginUser", userInfo);
-      store.dispatch("modules/todos/fetchTodos");
+        uid: user.uid,
+      }
+      store.dispatch('modules/auth/setLoginUser', userInfo)
     } else {
-      return redirect("/signIn")
-
+      if (route.path === '/signIn') {
+        return redirect('/')
+      } else {
+        return redirect('/signIn')
+      }
     }
-  });
+  })
 }
 
