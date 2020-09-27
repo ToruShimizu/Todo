f3c5bd0aa29655bc699abf4a69cdff75f3aa3f74
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-09-27 09:21:17 +0900

    Add: todosが0の時にfirestoreから取得するようい処理を追加

--- Todo-List/pages/index.vue
+++ Todo-List/pages/index.vue
@@ -8,11 +8,22 @@
 
 <script>
 import Todos from '@/components/Todos/Todos'
+import { mapState, mapActions } from 'vuex'
 export default {
   middleware: 'authenticated',
-
   components: {
     Todos,
+  },
+  created() {
+    if (this.todos.length === 0) {
+      this.fetchTodos()
+    }
+  },
+  computed: {
+    ...mapState('modules/todos', ['todos']),
+  },
+  methods: {
+    ...mapActions('modules/todos', ['fetchTodos']),
   },
 }
 </script>
