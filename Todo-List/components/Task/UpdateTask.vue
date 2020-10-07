4980f946c8a5946d2e2a35bddad146dc7755388d
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-07 20:04:19 +0900

    #95 Fix: タスクのタイトルにv-modelが設定されていなかったため修正

--- Todo-List/components/Task/UpdateTask.vue
+++ Todo-List/components/Task/UpdateTask.vue
@@ -1,87 +1,86 @@
 <template>
   <v-app>
-    <v-col cols="12" sm="12" md="12">
-      <v-card>
-        <v-card-title>
-          <span class="headline">タスク詳細×編集</span>
-        </v-card-title>
-        <v-form ref="form" lazy-validation>
-          <v-container>
-            <v-row>
-              <!-- タスク編集エリア -->
-              <v-col cols="12">
-                <v-text-field
-                  v-model="editTitle"
-                  label="タスクを変更する"
-                  prepend-inner-icon="mdi-pencil-outline"
-                  :rules="titleRules"
-                  clearable
-                />
-              </v-col>
-              <!-- 日付編集エリア -->
-              <v-col cols="12">
-                <v-menu
-                  ref="menu"
-                  v-model="updateDateMenu"
-                  :close-on-content-click="false"
-                  :return-value.sync="editDate"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
+      <v-col cols="12" sm="12" md="12">
+        <v-card>
+          <v-card-title>
+            <span class="headline">タスク詳細×編集</span>
+          </v-card-title>
+          <v-form ref="form" lazy-validation>
+            <v-container>
+              <v-row>
+                <!-- タスク編集エリア -->
+                <v-col cols="12">
+                  <v-text-field
+                    label="タスクを変更する"
+                    prepend-inner-icon="mdi-pencil-outline"
+                    :rules="titleRules"
+                    clearable
+                  />
+                </v-col>
+                <!-- 日付編集エリア -->
+                <v-col cols="12">
+                  <v-menu
+                    ref="menu"
+                    v-model="updateDateMenu"
+                    :close-on-content-click="false"
+                    :return-value.sync="editDate"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="editDate"
+                        label="期限を変更する"
+                        prepend-inner-icon="mdi-calendar-today"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker  no-title scrollable>
+                      <v-spacer></v-spacer>
+                      <v-btn text color="primary" @click="updateDateMenu = false"
+                        >Cancel</v-btn
+                      >
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.menu.save(editDate)"
+                        >OK</v-btn
+                      >
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
+                <!-- 詳細編集エリア -->
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="editDetail"
+                    label="タスクの詳細を変更する"
+                    prepend-inner-icon="mdi-briefcase-outline"
+                    required
+                    clearable
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <AddComment />
+                </v-col>
+                <Comment />
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="$emit('close-update-task')"
+                  >Cancel</v-btn
                 >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="editDate"
-                      label="期限を変更する"
-                      prepend-inner-icon="mdi-calendar-today"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker  no-title scrollable>
-                    <v-spacer></v-spacer>
-                    <v-btn text color="primary" @click="updateDateMenu = false"
-                      >Cancel</v-btn
-                    >
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="$refs.menu.save(editDate)"
-                      >OK</v-btn
-                    >
-                  </v-date-picker>
-                </v-menu>
-              </v-col>
-              <!-- 詳細編集エリア -->
-              <v-col cols="12">
-                <v-text-field
-                  v-model="editDetail"
-                  label="タスクの詳細を変更する"
-                  prepend-inner-icon="mdi-briefcase-outline"
-                  required
-                  clearable
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <AddComment />
-              </v-col>
-              <Comment />
-            </v-row>
-          </v-container>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="$emit('close-update-task')"
-                >Cancel</v-btn
-              >
-            <v-btn color="blue darken-1" text @click="updateTask">Save</v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-col>
+              <v-btn color="blue darken-1" text @click="updateTask">Save</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
   </v-app>
 </template>
 
