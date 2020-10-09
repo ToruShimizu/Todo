40f37e15aa4485ceb10ba0beb3e3ff067fb418c0
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-09 20:23:02 +0900

    #98 Add:　タスクごとのコメントを取得するためidを取得処理の引数に渡す

--- Todo-List/components/Comment/Comment.vue
+++ Todo-List/components/Comment/Comment.vue
@@ -9,7 +9,7 @@
         </v-list-item-icon>
         <v-list-item-title>
           {{ userName }}さんがコメントしました
-        </v-list-item-title>
+          </v-list-item-title>
         <v-layout>
           <v-text-field v-model="comment.message" />
           <v-btn icon @click="removeComment(comment)">
@@ -22,11 +22,15 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
+  created() {
+    const id = this.editTodo.task.id
+    this.fetchComments(id)
+  },
   computed: {
-    ...mapState("modules/todos",["comments"]),
+    ...mapState("modules/todos",["comments", "editTodo"]),
     ...mapGetters("modules/auth",["userName", "photoURL", "userEmail"]),
   },
   methods: {
@@ -34,6 +38,7 @@ export default {
       if (!confirm('Are you sure?')) return
       this.$store.dispatch('modules/todos/removeComment', { id: comment.id })
     },
+    ...mapActions('modules/todos', ['fetchComments']),
   },
 }
 </script>
