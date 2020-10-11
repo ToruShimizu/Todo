b946c18d554554fd132f8cd7c936a1b68db75339
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-11 12:13:22 +0900

    Add: ルール追加

--- Todo-List/.eslintrc.js
+++ Todo-List/.eslintrc.js
@@ -12,26 +12,27 @@ module.exports = {
     'standard',
     'prettier',
     'prettier/vue',
-    "eslint:recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:prettier/recommended'
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
     'vue/max-attributes-per-line': 'off',
-    "semi": [2, "never"],
-    // "prettier.semi":false,
-    "prettier/prettier": ["error", { "semi": false }],
-    "dot-notation": [2, {"allowPattern": "^[a-z]+(_[a-z]+)+$"}],
-    "no-console": "off",
+    semi: [2, 'never'],
+    'no-duplicate-imports': 0,
+    'import/no-duplicates': 2,
 
+    // "prettier.semi":false,
+    'prettier/prettier': ['error', { semi: false }],
+    'dot-notation': [2, { allowPattern: '^[a-z]+(_[a-z]+)+$' }],
+    'no-console': 'off',
 
     // 文字列はシングルクオートのみ
-    "quotes": [2, "single"],
+    quotes: [2, 'single'],
     singleQuote: true,
     trailingComma: true,
     printWidth: 100
   }
-  }
+}
