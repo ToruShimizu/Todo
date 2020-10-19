c371a0746306da25ae078d22f22999431051d400
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-19 14:26:04 +0900

    #118 Fix: filters非推奨のため、時刻成形をcomputedで定義するよう修正

--- Todo-List/components/Comment/Comment.vue
+++ Todo-List/components/Comment/Comment.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list subheader>
-    <v-list-item v-for="comment in comments" :key="comment.id">
+    <v-list-item>
       <v-list-item-content>
         <v-list-item-icon v-if="photoURL">
           <v-avatar size="50">
@@ -8,7 +8,7 @@
           </v-avatar>
         </v-list-item-icon>
         <v-list-item-title> {{ userName }}さんがコメントしました</v-list-item-title>
-        <v-list-item-title> 投稿日時:{{ comment.created.toDate() | dateFilter }}</v-list-item-title>
+        <v-list-item-title> {{ commentCreateTime }}</v-list-item-title>
         <v-layout>
           <v-text-field v-model="comment.message" />
           <v-btn icon @click="removeComment(comment)">
@@ -25,14 +25,17 @@ import { mapState, mapGetters, mapActions } from 'vuex'
 import moment from 'moment'
 
 export default {
+  props: {
+    comment: {
+      type: Object
+    }
+  },
   computed: {
+    commentCreateTime(comment) {
+      return moment(this.comment.created.toDate()).format('YYYY年MM月DD日 HH時mm分ss秒')
+    },
     ...mapState('modules/todos', ['comments']),
     ...mapGetters('modules/auth', ['userName', 'photoURL', 'userEmail'])
-  },
-  filters: {
-    dateFilter(date) {
-      return moment(date).format('YYYY年MM月DD日 HH時mm分ss秒')
-    }
   },
   methods: {
     removeComment(comment) {
