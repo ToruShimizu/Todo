59e328b20217787759919929018f0c3d079d95f5
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-27 22:04:11 +0900

    Fix: ログイン画面のスタイルを修正

--- pages/signIn.vue
+++ pages/signIn.vue
@@ -1,14 +1,12 @@
 <template>
-  <v-app class="mt-12">
+  <v-app>
     <v-col cols="12" sm="12" md="12">
-      <v-card width="400px" class="mx-auto mt-5 text-center">
-        <v-card-title>
-          <h1>Todo</h1>
-          <p>
-            ご利用の方は、
-            <br />こちらからログインしてください。
-          </p>
+      <v-card width="400px" class="mx-auto">
+        <v-card-title class="text-center">
+          <h4 class="fill-width">ログインはこちらから</h4>
         </v-card-title>
+        <v-divider />
+
         <v-card-text>
           <v-btn
             class="fill-width text-capitalize caption"
@@ -36,9 +34,10 @@
             ><v-icon>mdi-account-arrow-left-outline </v-icon> テストユーザーでログイン
           </v-btn>
         </v-card-text>
-        <v-card-title>
-          <h2>ログイン</h2>
-        </v-card-title>
+        <v-divider />
+        <div class="separator separator_login_page">
+          <div class="middle_separator">または</div>
+        </div>
         <v-card-text>
           <v-form ref="form" lazy-validation @submit.prevent="login">
             <v-text-field
@@ -58,7 +57,7 @@
               @click:append="showPassword = !showPassword"
               counter="72"
             />
-            <v-btn text color="primary accent-4" @click="openResetPassword">
+            <v-btn text color="primary accent-4" @click="openResetPassword" class="fill-width">
               パスワードを忘れた方はこちら</v-btn
             >
             <v-card-actions>
@@ -92,14 +91,13 @@
 
 <script>
 import { mapActions } from 'vuex'
-import CreateUser from '@/components/CreateUser/CreateUser'
-import ResetPassword from '@/components/ResetPassword/ResetPassword'
 import FormValidation from '../mixins/FormValidation.vue'
 import LoadingView from '../mixins/LoadingView.vue'
+import CreateUser from '@/components/CreateUser/CreateUser'
+import ResetPassword from '@/components/ResetPassword/ResetPassword'
 
 export default {
   mixins: [FormValidation, LoadingView],
-
   components: {
     CreateUser,
     ResetPassword
@@ -149,3 +147,39 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.fill-width {
+  width: 100%;
+}
+.link-caption {
+  text-decoration: none;
+  color: #666 !important;
+  font-size: 0.75rem;
+}
+.separator {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  height: 0;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #fff;
+  position: relative;
+}
+.middle_separator {
+  position: absolute;
+  padding: 0 16px;
+  color: #ccc;
+  background: #fff;
+  font-size: 11px;
+  text-shadow: 0 1px 0 #fff;
+  text-transform: uppercase;
+  top: -7px;
+  left: 30%;
+}
+.middle_separator {
+  color: #777;
+  font-size: 13px;
+  top: -9px;
+  left: 41%;
+}
+</style>
