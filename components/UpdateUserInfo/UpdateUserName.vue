19fadb658ce6887d8af655177c0154015156f72e
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-27 22:32:52 +0900

    Fix: メールアドレス変更画面のスタイルを修正

--- components/UpdateUserInfo/UpdateUserName.vue
+++ components/UpdateUserInfo/UpdateUserName.vue
@@ -8,35 +8,40 @@
     >
       <v-app>
         <v-col cols="12" sm="12" md="12">
-          <v-card width="400px" class="mx-auto mt-5 text-center">
-            <v-card-title>
-              <h2>ユーザー情報編集</h2>
+          <v-card width="400px" class="mx-auto text-center">
+            <v-card-title class="text-center">
+              <!-- FIXME classはscssに記述する -->
+              <h4 class="fill-width">メールアドレス変更</h4>
             </v-card-title>
-            <v-card-text>
-              <v-form ref="form" lazy-validation @submit.prevent="updateUserName">
-                <v-text-field
-                  v-model="editUserName"
-                  prepend-inner-icon="mdi-card-account-details-outline"
-                  label="名前を変更する"
-                  :rules="[validRules.nameRules.required]"
-                  clearable
-                />
-                <v-card-actions>
-                  <v-btn
-                    color="success"
-                    @click="updateUserName"
-                    :loading="loadingUpdateUserName"
-                    :disabled="loadingUpdateUserName"
-                  >
-                    <v-icon left>mdi-account</v-icon>SAVE
-                  </v-btn>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" @click="selectedUpdateUserName = 'closeUpdateUserName'">
-                    <v-icon left>mdi-login-variant</v-icon>戻る
-                  </v-btn>
-                </v-card-actions>
-              </v-form>
-            </v-card-text>
+            <v-card-title>
+              <v-card-text>
+                現在登録されているユーザー名<br />
+                <v-icon left>mdi-account-outline</v-icon>{{ userName }}
+              </v-card-text>
+            </v-card-title>
+            <v-form ref="form" lazy-validation @submit.prevent="updateUserName">
+              <v-text-field
+                v-model="editUserName"
+                prepend-inner-icon="mdi-card-account-details-outline"
+                label="新しい名前を入力してください"
+                :rules="[validRules.nameRules.required]"
+                clearable
+              />
+              <v-card-actions>
+                <v-btn color="primary" @click="selectedUpdateUserName = 'closeUpdateUserName'">
+                  <v-icon left>mdi-login-variant</v-icon>戻る
+                </v-btn>
+                <v-spacer />
+                <v-btn
+                  color="success"
+                  @click="updateUserName"
+                  :loading="loadingUpdateUserName"
+                  :disabled="loadingUpdateUserName"
+                >
+                  <v-icon left>mdi-account</v-icon>SAVE
+                </v-btn>
+              </v-card-actions>
+            </v-form>
           </v-card>
         </v-col>
       </v-app>
@@ -45,6 +50,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import FormValidation from '@/mixins/FormValidation.vue'
 import LoadingView from '@/mixins/LoadingView.vue'
 export default {
@@ -66,7 +72,8 @@ export default {
         this.$emit('update:selectedUpdateUserName', value)
         this.editUserName = ''
       }
-    }
+    },
+    ...mapGetters('modules/auth', ['userName'])
   },
   data() {
     return {
