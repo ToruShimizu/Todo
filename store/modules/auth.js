934ef161932be76bfc0efb37603a9a9a2949cc33
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-10-27 18:01:53 +0900

    #131 Add: onAuthStateChangedを定義して変更後に呼び出すよう処理を追加

--- store/modules/auth.js
+++ store/modules/auth.js
@@ -28,6 +28,19 @@ const actions = {
   deleteLoginUser({ commit }) {
     commit('deleteLoginUser')
   },
+  async onAuthStateChanged({dispatch}) {
+    await auth.onAuthStateChanged((user) => {
+      if (user) {
+        const userInfo = {
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          uid: user.uid
+        }
+        dispatch('setLoginUser', userInfo)
+      }
+    })
+  },
   // Googleログイン
   async googleLogin({ commit }) {
     try {
@@ -77,9 +90,8 @@ const actions = {
   },
   // ユーザー情報の更新
   // FIXME:ユーザーのstate更新
-  async updateUserName({ commit }, { userName }) {
-    const user = await firebase.auth().currentUser
-    console.log(user)
+  async updateUserName({ dispatch }, { userName }) {
+  const user = await firebase.auth().currentUser
     if (user.displayName === 'テストユーザー') {
       alert('テストユーザーは変更できません')
     } else {
@@ -88,7 +100,8 @@ const actions = {
           displayName: userName
         })
         alert('成功しました')
-        commit('setLoginUser')
+        console.log(user)
+        dispatch('onAuthStateChanged')
       } catch (err) {
         alert('更新に失敗しました')
         console.log(err)
@@ -96,7 +109,7 @@ const actions = {
     }
   },
   // メールアドレスの変更
-  async updateEmailAddress({ commit }, { email }) {
+  async updateEmailAddress({ dispatch }, { email }) {
     const user = firebase.auth().currentUser
     if (user.email === 'test@example.com') {
       alert('テストユーザーは変更できません')
@@ -104,7 +117,7 @@ const actions = {
       try {
         await user.updateEmail(email)
         alert('新しいメールアドレスの登録が完了しました')
-        commit('setLoginUser')
+        dispatch('onAuthStateChanged')
       } catch (err) {
         alert('新しいメールアドレスの登録に失敗しました')
         console.log(err)
