61191993bfc62289849fabd792ff033372e5118f
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-03 13:33:24 +0900

    Add: footerにfixedを指定

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app dark>
     <Header />
     <nuxt />
-    <v-footer color="indigo darken-2" class="absolute">
+    <v-footer color="indigo darken-2" fixed>
       <span class="grey--text text--lighten-2">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
