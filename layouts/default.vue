66243c40898bb2ea983032280c02f3ad2f3bf5f0
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-03 13:13:40 +0900

    Fix: absoluteでfooterを最下部に固定

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app dark>
     <Header />
     <nuxt />
-    <v-footer color="indigo darken-2">
+    <v-footer color="indigo darken-2" class="absolute">
       <span class="grey--text text--lighten-2">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
