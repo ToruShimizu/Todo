0ad869abd1d18efc7d13c0695329a6e4087bbf45
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-06 17:11:13 +0900

    #153 Add: 絞り込み、検索機能、ページごとの切り分け機能をTaskTable.vueより移行

--- components/Todos/Todos.vue
+++ components/Todos/Todos.vue
@@ -24,14 +24,15 @@
       <v-divider />
       <v-layout>
         <SearchTask :search.sync="searchTaskKeyword" />
-        <SortByTask :selected.sync="selectSortTask" />
+        <SortByTask ref="sortByTask" :selected.sync="selectSortTask" :searchTask="searchTask" />
       </v-layout>
       <TaskTable
-        :searchTaskKeyword="searchTaskKeyword"
-        :taskFilter="taskFilter"
-        :selectSortTask="selectSortTask"
-        :sortTaskTitleOrder="sortTaskTitleOrder"
-        :sortTaskDateOrder="sortTaskDateOrder"
+        ref="taskTable"
+        :sortByTask="sortByTask"
+        :todosPage.sync="todosPage"
+        :todosPageSize="todosPageSize"
+        :todosFiltered="todosFiltered"
+        @change-todos-page="changeTodosPage"
       />
     </v-card>
   </v-container>
@@ -61,19 +62,103 @@ export default {
         date: new Date().toISOString(),
         done: false
       },
-      selectSortTask: 'ascDate',
-      sortTaskTitleOrder: 1,
-      sortTaskDateOrder: 1,
-      taskFilter: 'all',
+      todosPage: 1,
+      todosPageSize: 7,
       searchTaskKeyword: '',
+      selectSortTask: 'ascDate',
+      taskFilter: 'all',
       taskDialog: false
     }
   },
   computed: {
+    todoList: {
+      get() {
+        // 絞り込が行われたあとのデータを使用
+        const todos = this.todosFiltered()
+        // 1ページあたりの最大表示数に合わせて切り分ける
+        return todos.slice(
+          this.todosPageSize * (this.todosPage - 1),
+          this.todosPageSize * this.todosPage
+        )
+      },
+      set(changeTodosPage) {
+        return changeTodosPage
+      }
+    },
     ...mapGetters('modules/todos', ['todosCount']),
     ...mapState('modules/todos', ['todos'])
   },
   methods: {
+    // 完了状態の絞り込み
+    todosFiltered() {
+      let returnvalue
+      const todos = this.todos
+      switch (this.taskFilter) {
+        case 'all':
+          returnvalue = todos
+          console.log(this.taskFilter)
+          break
+        case 'active':
+          returnvalue = todos.filter((todo) => !todo.task.done)
+          console.log(this.taskFilter)
+
+          break
+        case 'done':
+          returnvalue = todos.filter((todo) => todo.task.done)
+          console.log(this.taskFilter)
+
+          break
+        default:
+      }
+      return returnvalue
+    },
+    // タスクの検索
+    searchTask() {
+      // vuetifyのclearableを使用するとnullになり表示されなくなるためnullの場合の処理を記述
+      // nullの場合は元の値であるstateのtodosを入れて返す
+      if (this.searchTaskKeyword === null) {
+        this.todoList = this.todos
+        return this.todoList
+      }
+      return this.todoList.filter((todo) => {
+        return todo.task.title.includes(this.searchTaskKeyword)
+      })
+    },
+    sortByTask() {
+      let returnvalue
+      // SortByTaskコンポーネントのメソッドを呼び出す
+      const ref = this.$refs.sortByTask
+
+      switch (this.selectSortTask) {
+        case 'title':
+          returnvalue = ref.sortByTaskTitle()
+          console.log(this.selectSortTask)
+          break
+        case 'ascDate':
+          returnvalue = ref.sortByAscDate()
+          console.log(this.selectSortTask)
+
+          break
+        case 'descDate':
+          returnvalue = ref.sortByDescDate()
+
+          console.log(this.selectSortTask)
+
+          break
+        default:
+      }
+      return returnvalue
+    },
+    // ページ番号のボタンが押された時にページを切り替える
+    changeTodosPage(pageNumber) {
+      const todos = this.todosFiltered()
+      this.todoList = todos.slice(
+        this.todosPageSize * (pageNumber - 1),
+        this.todosPageSize * pageNumber
+      )
+      console.log(pageNumber)
+      console.log('page', this.todosPage)
+    },
     openAddTask() {
       this.taskDialog = true
     },
