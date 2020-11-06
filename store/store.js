53aaabdcea9ffa911d1874b0758e5666cbdce88f
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-07 06:28:08 +0900

    #162 Add: modulesのcomment.jsを追加

--- store/store.js
+++ store/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as auth from './modules/auth'
 import * as todos from './modules/todos'
+import * as comment from './modules/comment'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     auth,
-    todos
+    todos,
+    comment
   }
 })
