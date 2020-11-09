156fabba730f7f66072a04acf29c9d015df58e51
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-09 19:34:39 +0900

    #208 Fix: EsLintによるコードの並び順の修正

--- components/ResetPassword/ResetPassword.vue
+++ components/ResetPassword/ResetPassword.vue
@@ -27,18 +27,18 @@
                   clearable
                 />
                 <v-card-actions>
-                  <v-btn color="primary" @click="closeResetPasswprd" class="hidden-sm-and-up">
+                  <v-btn color="primary" class="hidden-sm-and-up" @click="closeResetPasswprd">
                     <v-icon left>mdi-login-variant</v-icon>戻る
                   </v-btn>
-                  <v-btn color="primary" @click="closeResetPasswprd" class="hidden-xs-only">
+                  <v-btn color="primary" class="hidden-xs-only" @click="closeResetPasswprd">
                     <v-icon left>mdi-login-variant</v-icon>ログイン画面へ戻る
                   </v-btn>
                   <v-spacer />
                   <v-btn
                     color="success"
-                    @click="passwordReset"
                     :loading="loadingResetPassword"
                     :disabled="loadingResetPassword"
+                    @click="passwordReset"
                   >
                     <v-icon left>mdi-email-send</v-icon>送信
                   </v-btn>
