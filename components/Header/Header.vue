41ac9d436a7e1117ad9b8e1ed8bc856cff7adcc9
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-09 17:45:55 +0900

    #198 Remove: 不要なコードを削除

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,18 +1,16 @@
 <template>
   <v-app>
-    <header>
-      <v-navigation-drawer v-model="sideNavDrawer" app clipped>
-        <SideNav />
-      </v-navigation-drawer>
-      <v-app-bar color="indigo darken-2" dark app clipped-left>
-        <v-app-bar-nav-icon @click="sideNavDrawer = !sideNavDrawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>ToDo</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items v-if="login_user">
-          <v-icon @click="logout"> mdi-account-arrow-right-outline </v-icon>
-        </v-toolbar-items>
-      </v-app-bar>
-    </header>
+    <v-navigation-drawer v-model="sideNavDrawer" app clipped>
+      <SideNav />
+    </v-navigation-drawer>
+    <v-app-bar color="indigo darken-2" dark app clipped-left>
+      <v-app-bar-nav-icon @click="sideNavDrawer = !sideNavDrawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>ToDo</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items v-if="login_user">
+        <v-icon @click="logout"> mdi-account-arrow-right-outline </v-icon>
+      </v-toolbar-items>
+    </v-app-bar>
   </v-app>
 </template>
 
