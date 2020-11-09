522597413f8d4728be9589cb649789384b669f45
Toru Shimizu <nobbyz.t.nn@gmail.com>
2020-11-09 19:03:53 +0900

    #203 Add: コメント表示全体に対してv-colでcolsを12に設定

--- components/Comment/Comment.vue
+++ components/Comment/Comment.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-card class="mx-auto my-2" width="80%" elevation="3">
-    <v-card-title>
-      <v-icon small left> mdi-comment-text-outline </v-icon>
-      <span class="subtitle-2 font-weight-light">コメント</span>
-      <v-spacer />
-      <span class="subtitle-2 font-weight-light">{{ comment.created }}</span>
-    </v-card-title>
+  <v-col cols="12">
+    <v-card class="mx-auto my-2" width="80%" elevation="3">
+      <v-card-title>
+        <v-icon small left> mdi-comment-text-outline </v-icon>
+        <span class="subtitle-2 font-weight-light">コメント</span>
+        <v-spacer />
+        <span class="subtitle-2 font-weight-light">{{ comment.created }}</span>
+      </v-card-title>
 
-    <v-card-text class="body-1 font-weight-bold">
-      "{{ comment.message }}"
-      <v-row align="center" justify="end">
-        <v-btn icon @click="removeComment(comment)">
-          <v-icon>mdi-delete</v-icon>
-        </v-btn>
-      </v-row>
-    </v-card-text>
-  </v-card>
+      <v-card-text class="body-1 font-weight-bold">
+        "{{ comment.message }}"
+        <v-row align="center" justify="end">
+          <v-btn icon @click="removeComment(comment)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-col>
 </template>
 
 <script>
