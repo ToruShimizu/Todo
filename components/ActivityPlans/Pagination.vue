631b4067c528daf650d64c952c627609def57f90
Toru <Toru@Toru.local>
2020-11-15 00:00:33 +0900

    #300 Change: 活動計画を入力する仕様に変更したため該当箇所を変更

---
+++ components/ActivityPlans/Pagination.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center">
+    <v-pagination
+      v-model="activityPlansPageCount"
+      :length="activityPlansPageLength"
+      @input="changeActivityPlansPage"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activityPlansPage: {
+      type: Number,
+      required: false,
+      default: 1
+    },
+    activityPlansPageLength: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    activityPlansPageCount: {
+      get() {
+        return this.activityPlansPage
+      },
+      set(setActivityPlansPage) {
+        console.log(setActivityPlansPage)
+        this.$emit('update:set-activity-plans-page', setActivityPlansPage)
+      }
+    }
+  },
+  methods: {
+    changeActivityPlansPage(pageNumber) {
+      this.$emit('change-activity-plans-page', pageNumber)
+    }
+  }
+}
+</script>
