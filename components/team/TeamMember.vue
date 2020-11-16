cad074727228e7f000b743a32c2c8319020c9a6f
Toru <Toru@Toru.local>
2020-11-16 16:05:25 +0900

    #326 Add: メンバーリスト表示画面の作成

--- components/team/TeamMember.vue
+++ components/team/TeamMember.vue
@@ -12,39 +12,10 @@
             </tr>
           </thead>
           <tbody>
-            <template v-for="contents in displayActivityPlans">
-              <transition :key="contents.catgorys" name="list">
+            <template>
+              <transition>
                 <tr>
-                  <td>
-                    <v-btn icon @click="toggleDoneActivityPlan(contents)">
-                      <v-icon :class="(!contents.done && 'grey--text') || 'primary--text'"
-                        >mdi-check-circle-outline</v-icon
-                      >
-                    </v-btn>
-                  </td>
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                      <td
-                        v-bind="attrs"
-                        :class="(contents.done && 'grey--text') || 'primary--text'"
-                        v-on="on"
-                        @click="openUpdateActivityPlan(contents)"
-                      >
-                        {{ contents.category }}
-                      </td>
-                    </template>
-                    <span>{{ contents.category }}の詳細を開く</span>
-                  </v-tooltip>
-                  <td :class="(contents.done && 'grey--text') || 'black--text'" class="pa-0">
-                    {{ contents.date }}
-                  </td>
-                  <td>
-                    <v-btn icon>
-                      <v-icon @click="handleRemoveActivityPlan(contents)"
-                        >mdi-delete-outline</v-icon
-                      >
-                    </v-btn>
-                  </td>
+                  <td></td>
                 </tr>
               </transition>
             </template>
