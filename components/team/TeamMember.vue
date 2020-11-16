72dd75db32056b0cdcabda875a84d625581b49cb
Toru <Toru@Toru.local>
2020-11-16 23:11:52 +0900

    #334 Change:テーブル表示をデータテーブルに変更

--- components/team/TeamMember.vue
+++ components/team/TeamMember.vue
@@ -1,47 +1,66 @@
 <template>
-  <v-app>
-    <v-card>
-      <v-simple-table>
-        <template>
-          <thead>
-            <tr>
-              <th class="text-left">名前</th>
-              <th class="text-left">役割</th>
-              <th class="text-left">改善</th>
-              <th class="text-left">削除</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-for="member in teamMember">
-              <transition :key="member.name" name="list">
-                <tr>
-                  <td>{{ member.name }}</td>
-
-                  <td>
-                    {{ member.role }}
-                  </td>
-                  <td>{{ member.improvementRole }}</td>
-                  <td>
-                    <v-btn icon>
-                      <v-icon>mdi-delete-outline</v-icon>
-                    </v-btn>
-                  </td>
-                </tr>
-              </transition>
-            </template>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-card>
-  </v-app>
+  <v-layout class="justify-center">
+    <v-data-table
+      :headers="memberHeaders"
+      :items="teamMember"
+      :search="searchMember"
+      :options.sync="pageOptions"
+      sort-by="名前"
+      class="elevation-1"
+      style="width: 80%"
+    >
+      <template v-slot:top>
+        <v-toolbar flat dense>
+          <v-spacer />
+          <v-text-field
+            v-model="searchMember"
+            prepend-inner-icon="mdi-magnify"
+            label="検索"
+            clearable
+            single-line
+            hide-details
+          />
+          <v-divider class="mx-4" inset vertical />
+          <v-btn color="primary" dark>
+            <v-icon>mdi-account-plus-outline </v-icon>
+          </v-btn>
+          <v-spacer />
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-btn text x-small>
+          <v-icon small> mdi-pencil </v-icon>
+        </v-btn>
+        <v-btn text x-small>
+          <v-icon small> mdi-delete </v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-layout>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 export default {
-  data() {
-    return {}
-  },
+  data: () => ({
+    memberHeaders: [
+      {
+        text: '名前',
+        align: 'start',
+        sortable: true,
+        value: 'name'
+      },
+      { text: '役割', value: 'role', sortable: true },
+      { text: '改善', value: 'improvementRoles', sortable: true },
+      { text: '編集or削除', value: 'actions', sortable: false }
+    ],
+    searchMember: '',
+    pageOptions: {
+      page: 1,
+      itemsPerPage: 5
+    }
+  }),
+
   computed: {
     ...mapState('modules/team/team', ['teamMember'])
   }
