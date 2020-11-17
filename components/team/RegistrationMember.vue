4190a4e5d871e836568f5723b5209c7527608b84
Toru <Toru@Toru.local>
2020-11-17 09:19:28 +0900

    #310 Add: 登録時にバリデーションチェックを追加

--- components/team/RegistrationMember.vue
+++ components/team/RegistrationMember.vue
@@ -9,51 +9,54 @@
       <v-col cols="12" sm="12" md="12">
         <v-card width="600px" class="mx-auto">
           <v-card-text>
-            <v-row>
-              <v-col cols="12" sm="12" md="12">
-                <v-text-field
-                  v-model="teamMember.name"
-                  label="名前"
-                  required
-                  clearable
-                  dense
-                  prepend-inner-icon="mdi-card-account-details-outline"
-                  :rules="[validRules.nameRules.required]"
-                />
-              </v-col>
+            <v-form ref="form" lazy-validation>
+              <v-row>
+                <v-col cols="12" sm="12" md="12">
+                  <v-text-field
+                    ref="memberName"
+                    v-model="teamMember.name"
+                    label="名前"
+                    required
+                    clearable
+                    dense
+                    prepend-inner-icon="mdi-card-account-details-outline"
+                    :rules="[validRules.nameRules.required]"
+                  />
+                </v-col>
 
-              <v-col cols="12" sm="12" md="12">
-                <v-combobox
-                  v-model="teamMember.role"
-                  :items="teamRoles"
-                  label="サークル役割"
-                  clearable
-                  multiple
-                  small-chips
-                  persistent-hint
-                  dense
-                  prepend-inner-icon="mdi-briefcase-account-outline"
-                  hint="文字入力の場合はEnterキーを押してください"
-                  @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
-                >
-                </v-combobox>
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <v-combobox
-                  v-model="teamMember.improvementRole"
-                  :items="improvementRoles"
-                  label="改善事例担当"
-                  clearable
-                  persistent-hint
-                  hint="文字入力の場合はEnterキーを押してください"
-                  multiple
-                  small-chips
-                  prepend-inner-icon="mdi-briefcase-outline"
-                  @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
-                >
-                </v-combobox>
-              </v-col>
-            </v-row>
+                <v-col cols="12" sm="12" md="12">
+                  <v-combobox
+                    v-model="teamMember.role"
+                    :items="teamRoles"
+                    label="サークル役割"
+                    clearable
+                    multiple
+                    small-chips
+                    persistent-hint
+                    dense
+                    prepend-inner-icon="mdi-briefcase-account-outline"
+                    hint="文字入力の場合はEnterキーを押してください"
+                    @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
+                  >
+                  </v-combobox>
+                </v-col>
+                <v-col cols="12" sm="12" md="12">
+                  <v-combobox
+                    v-model="teamMember.improvementRole"
+                    :items="improvementRoles"
+                    label="改善事例担当"
+                    clearable
+                    persistent-hint
+                    hint="文字入力の場合はEnterキーを押してください"
+                    multiple
+                    small-chips
+                    prepend-inner-icon="mdi-briefcase-outline"
+                    @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
+                  >
+                  </v-combobox>
+                </v-col>
+              </v-row>
+            </v-form>
           </v-card-text>
           <v-card-actions class="justify-end">
             <v-btn color="primary" @click="closeRegistrationMmber" text>close</v-btn>
@@ -93,9 +96,14 @@ export default {
   methods: {
     handleRegistrationMember() {
       const teamMember = this.teamMember
+      if (!teamMember.name) {
+        this.$refs.form.validate()
+        return
+      }
       this.registrationMember(teamMember)
     },
     closeRegistrationMmber() {
+      this.$refs.memberName.reset()
       this.$emit('close-registration-member')
     },
     ...mapActions('modules/team/team', ['registrationMember'])
