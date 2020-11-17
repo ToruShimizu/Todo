f78dcadf584d215d820a983608dffe5a1fa43798
Toru <Toru@Toru.local>
2020-11-17 22:17:42 +0900

    #353 Add: サークルを編集するための選択リストを作成

--- components/team/TeamName.vue
+++ components/team/TeamName.vue
@@ -10,16 +10,18 @@
       </v-img>
       <v-icon v-else>mdi-account-outline</v-icon>
     </v-avatar>
-    <h2 class="grey--text font-italic">{{ teamName }}</h2>
-    <v-btn 　color="success" to="/createTeam" nuxt　class="ml-2">
-      <v-icon>mdi-account-multiple-plus </v-icon>
-    </v-btn>
+    <h2 class="grey--text font-italic mb-4">{{ teamName }}</h2>
+    <EditTeam />
   </v-layout>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import EditTeam from '@/components/team/EditTeam'
 export default {
+  components: {
+    EditTeam
+  },
   computed: {
     ...mapGetters('modules/team/team', ['teamName']),
     ...mapGetters('modules/user/auth', ['photoURL'])
---
+++ components/team/editTeam.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-menu transition="fab-transition">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn dark color="grey" v-bind="attrs" v-on="on" text>
+        <v-icon>mdi-pen </v-icon>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item>
+        <v-btn text>
+          <v-list-item-title @click="openEditTeam">サークル編集</v-list-item-title>
+        </v-btn>
+      </v-list-item>
+      <v-list-item>
+        <v-btn text>
+          <v-list-item-title @click="editDeleteTeam">サークル削除</v-list-item-title>
+        </v-btn>
+      </v-list-item>
+      <v-list-item v-if="this.$route.path !== '/createTeam'">
+        <v-btn to="/createTeam" nuxt text>
+          <v-list-item-title>メンバー編集</v-list-item-title>
+        </v-btn>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
