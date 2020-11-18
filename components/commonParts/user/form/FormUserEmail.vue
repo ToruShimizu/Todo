a64376152ae0281f693df90ac7a03724eb1aae48
Toru <Toru@Toru.local>
2020-11-19 00:12:36 +0900

    #372 Add: メールアドレス入力フォームのコンポーネントを作成

---
+++ components/commonParts/user/form/FormUserEmail.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-text-field
+    v-model="innerEmail"
+    prepend-inner-icon="mdi-email-edit-outline"
+    :label="emailLabel"
+    :rules="[validRules.emailRules.required, validRules.emailRules.regex]"
+    clearable
+  />
+</template>
+
+<script>
+import FormValidation from '@/mixins/FormValidation.vue'
+
+export default {
+  mixins: [FormValidation],
+  props: {
+    email: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    emailLabel: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+
+  computed: {
+    innerEmail: {
+      get() {
+        return this.email
+      },
+      set(userEmail) {
+        this.$emit('update:user-email', userEmail)
+      }
+    }
+  }
+}
+</script>
