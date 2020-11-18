20af62acc3ed227fd1f61ae264e41715ad02a257
Toru <Toru@Toru.local>
2020-11-18 12:03:21 +0900

    #344 Add: サークル編集用のコンポーネントを追加

--- components/team/edit/EditTeamList.vue
+++ components/team/edit/EditTeamList.vue
@@ -1,29 +1,45 @@
 <template>
-  <v-menu transition="fab-transition">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn dark color="grey" v-bind="attrs" v-on="on" text>
-        <v-icon>mdi-pen </v-icon>
-      </v-btn>
-    </template>
-    <v-list>
-      <v-list-item>
-        <v-btn text>
-          <v-list-item-title @click="openEditTeam">サークル編集</v-list-item-title>
+  <div>
+    <v-menu transition="fab-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn dark color="grey" v-bind="attrs" v-on="on" text>
+          <v-icon>mdi-pen </v-icon>
         </v-btn>
-      </v-list-item>
-      <v-list-item>
-        <v-btn text>
-          <v-list-item-title @click="handleDeleteTeam">サークル削除</v-list-item-title>
-        </v-btn>
-      </v-list-item>
-    </v-list>
-  </v-menu>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-btn text>
+            <v-list-item-title @click="openUpdateTeam">サークル編集</v-list-item-title>
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text>
+            <v-list-item-title @click="handleDeleteTeam">サークル削除</v-list-item-title>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <UpdateTeam :update-team-dialog="updateTeamDialog" @close-update-team="closeUpdateTeam" />
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import UpdateTeam from '@/components/team/edit/UpdateTeam'
 export default {
+  data() {
+    return {
+      updateTeamDialog: false
+    }
+  },
   methods: {
+    openUpdateTeam() {
+      console.log('hoge')
+      this.updateTeamDialog = true
+    },
+    closeUpdateTeam() {
+      this.updateTeamDialog = false
+    },
     handleDeleteTeam() {
       if (!confirm('サークルを削除しますか？')) return
       this.removeTeam()
