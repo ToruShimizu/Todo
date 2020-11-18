7ff37d530840f7f3ddceb65f2ebec4d7a99416c3
Toru <Toru@Toru.local>
2020-11-19 00:32:00 +0900

    #373 Add:saveボタンをコンポーネント化

---
+++ components/commonParts/button/SaveButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn color="blue darken-1" outlined @click="handleSaveButton"
+    ><span class="font-italic">{{ title }}</span></v-btn
+  >
+</template>
+
+<script>
+export default {
+  props: { title: String },
+  methods: {
+    handleSaveButton() {
+      this.$emit('save-button')
+    }
+  }
+}
+</script>
