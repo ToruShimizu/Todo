fd8a6d85ce1cc6507015b36b8abdae326301e055
Toru <Toru@Toru.local>
2020-11-18 22:24:04 +0900

    #368 Add: コメントの入力フォームにhintを追加

--- components/Comment/AddComment.vue
+++ components/Comment/AddComment.vue
@@ -2,12 +2,13 @@
   <v-col cols="12">
     <v-text-field
       v-model="message"
-      hide-details
+      persistent-hint
       prepend-inner-icon="mdi-message-text-outline"
+      hint="入力後はEnterキーを押してください"
       label="コメントを追加する"
       clearable
       @keypress.enter="handleAddComment"
-    ></v-text-field>
+    />
   </v-col>
 </template>
 
