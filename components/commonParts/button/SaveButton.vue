fbcd1a92a3ff6ee4229d5ee00e38fc9c673c24d3
Toru <Toru@Toru.local>
2020-11-19 02:02:04 +0900

    #373 fix: タイポミスを修正

--- components/commonParts/button/SaveButton.vue
+++ components/commonParts/button/SaveButton.vue
@@ -1,6 +1,6 @@
 <template>
   <v-btn color="blue darken-1" outlined @click="handleSaveButton">
-    <v-slot name="saveButton"></v-slot>
+    <slot name="saveButton"> </slot>
     <span class="font-italic">{{ title }} </span>
   </v-btn>
 </template>
