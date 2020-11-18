7bee766737bb67bc676dbc4689d8fd22f68ea568
Toru <Toru@Toru.local>
2020-11-19 02:27:49 +0900

    #378 Fix: ボタンコンポーネントにtitleを追加して任意の表示名を付けれるように修正

--- components/commonParts/button/SaveAndCloseButton.vue
+++ components/commonParts/button/SaveAndCloseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card-actions class="justify-end">
-    <CloseButton :title="'CLOSE'" @close-button="closeButton" />
-    <SaveButton :title="'SAVE'" @save-button="saveButton"
+    <CloseButton :title="closeButtonTitle" @close-button="closeButton" />
+    <SaveButton :title="saveButtonTitle" @save-button="saveButton"
       ><template v-slot:saveButton><slot name="save"></slot></template
     ></SaveButton>
   </v-card-actions>
@@ -11,6 +11,16 @@
 import SaveButton from '@/components/commonParts/button/SaveButton'
 import CloseButton from '@/components/commonParts/button/CloseButton'
 export default {
+  props: {
+    closeButtonTitle: {
+      type: String,
+      required: true
+    },
+    saveButtonTitle: {
+      Type: String,
+      required: true
+    }
+  },
   components: {
     SaveButton,
     CloseButton
