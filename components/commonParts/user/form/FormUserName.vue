533bea915956dcd526e327e2738dafb4558cdc9a
Toru <Toru@Toru.local>
2020-11-19 00:12:59 +0900

    #372 Add: ユーザー名の入力フォームを作成

---
+++ components/commonParts/user/form/FormUserName.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-text-field
+    v-model="innerName"
+    prepend-inner-icon="mdi-card-account-details-outline"
+    :label="nameLabel"
+    :rules="[validRules.nameRules.required]"
+    clearable
+  />
+</template>
+<script>
+import FormValidation from '@/mixins/FormValidation.vue'
+
+export default {
+  mixins: [FormValidation],
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    nameLabel: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    innerName: {
+      get() {
+        return this.name
+      },
+      set(userName) {
+        this.$emit('update:user-name', userName)
+      }
+    }
+  }
+}
+</script>
