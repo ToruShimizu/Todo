76fe06d05116d8b1ea3afb8f51a4fdd81111f01b
Toru <Toru@Toru.local>
2020-11-19 03:30:42 +0900

    #389 Add: フォーム画面のコンポーネントを作成

---
+++ components/commonParts/form/FormView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-col cols="12" sm="12" md="12">
+      <v-card width="400px" class="mx-auto">
+        <v-card-title class="justify-center font-italic grey--text darken">
+          <span class="headline">{{ title }}</span>
+        </v-card-title>
+        <v-divider />
+        <v-card-text>
+          <slot name="form"></slot>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: { title: String }
+}
+</script>
