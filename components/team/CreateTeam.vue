81fff8cb8ea1018c2ce7570056e922de0cf03c37
Toru <Toru@Toru.local>
2020-11-18 15:56:24 +0900

    #363 Add: サークル新規作成用のコンポーネントを作成

---
+++ components/team/CreateTeam.vue
@@ -0,0 +1,34 @@
+<template>
+  <FormCreateTeam
+    :title="'サークル新規作成'"
+    :team="createTeam"
+    :team-dialog="createTeamDialog"
+    @save-team="handleCreateTeam"
+    @close-team-dialog="closeTeamDialog"
+  />
+</template>
+<script>
+import FormCreateTeam from '@/components/commonParts/team/form/FormCreateTeam'
+export default {
+  props: {
+    createTeamDialog: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      createTeam: {
+        name: '',
+        imageFile: null
+      }
+    }
+  },
+  methods: {
+    handleCreateTeam() {},
+    closeTeamDialog() {
+      this.$emit('close-team-dialog')
+    }
+  }
+}
+</script>
