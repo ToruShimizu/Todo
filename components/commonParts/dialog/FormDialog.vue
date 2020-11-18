c6bca7ce1b58f6effb7f6bfe55a9237e2adb4510
Toru <Toru@Toru.local>
2020-11-19 05:44:02 +0900

    #388 Add: フォームのダイアログコンポーネントを作成

---
+++ components/commonParts/dialog/FormDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-dialog v-model="formDialog" persistent max-width="600px" transition="scroll-y-transition">
+    <v-app>
+      <slot name="dialog"></slot>
+    </v-app>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    formDialog: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
