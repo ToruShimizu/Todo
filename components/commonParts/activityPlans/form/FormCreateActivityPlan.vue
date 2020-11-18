f87c44616ce185b91c5d62cfb3d0aab5c7145bab
Toru <Toru@Toru.local>
2020-11-18 17:48:38 +0900

    #366 Add:活動計画作成フォームをコンポーネント化

---
+++ components/commonParts/activityPlans/form/FormCreateActivityPlan.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-dialog
+    v-model="activityPlanDialog"
+    persistent
+    max-width="600px"
+    transition="scroll-y-transition"
+  >
+    <v-app>
+      <v-col cols="12" sm="12" md="12">
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ title }}</span>
+          </v-card-title>
+          <v-form ref="form" lazy-validation>
+            <v-container>
+              <v-row>
+                <!-- 活動計画入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-combobox
+                    ref="category"
+                    v-model="planContents.category"
+                    :items="categorys"
+                    label="やること"
+                    dense
+                    :rules="[validRules.categoryRules.required]"
+                  />
+                </v-col>
+
+                <!-- 日付入力エリア -->
+                <v-col cols="12" sm="6" md="6">
+                  <v-menu
+                    v-model="dateMenu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="planContents.date"
+                        label="日付"
+                        prepend-inner-icon="mdi-calendar-today"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="planContents.date" no-title @input="dateMenu = false" />
+                  </v-menu>
+                </v-col>
+                <!-- 詳細入力エリア -->
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="planContents.detail"
+                    label="詳細"
+                    prepend-inner-icon="mdi-briefcase-outline"
+                    clearable
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="closeActivityPlan">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="handleSaveActivityPlan">Save</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-app>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import FormValidation from '@/mixins/FormValidation.vue'
+
+export default {
+  mixins: [FormValidation],
+
+  props: {
+    title: String,
+    planContents: {
+      type: Object,
+      required: false,
+      default: () => ({
+        category: '',
+        detail: '',
+        date: new Date().toISOString().substr(0, 10),
+        done: false
+      })
+    },
+    categorys: {
+      type: Array,
+      required: true
+    },
+    activityPlanDialog: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dateMenu: false
+    }
+  },
+  methods: {
+    async handleSaveActivityPlan() {
+      const planContents = this.planContents
+      if (planContents.category === '') {
+        this.$refs.form.validate()
+        return
+      }
+      this.$emit('save-activity-plan', planContents)
+    },
+    closeActivityPlan() {
+      this.$refs.category.reset()
+      this.planContents.category = []
+      this.planContents.detail = ''
+      this.planContents.date = new Date().toISOString().substr(0, 10)
+      this.$emit('close-activity-plan')
+    }
+  }
+}
+</script>
