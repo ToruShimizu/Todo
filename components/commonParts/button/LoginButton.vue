e01fbea4819803a79eda3203cf0586caf360afff
Toru <Toru@Toru.local>
2020-11-19 05:01:45 +0900

    #389 Add: ログイン画面で使用するボタンをコンポーネント化

---
+++ components/commonParts/button/LoginButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    height="48px"
+    outlined
+    width="100%"
+    class="text-capitalize caption"
+    style="border-color: #979797"
+    tile
+    @click="login"
+  >
+    <slot name="icon"></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$emit('login')
+    }
+  }
+}
+</script>
