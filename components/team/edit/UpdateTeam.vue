a4fb13b8d93a403eea5e6b1c89bed536873026a6
Toru <Toru@Toru.local>
2020-11-18 12:02:45 +0900

    #344 Add: サークル編集画面を作成

---
+++ components/team/edit/UpdateTeam.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-dialog
+    v-model="updateTeamDialog"
+    persistent
+    max-width="400px"
+    transition="scroll-y-transition"
+  >
+    <v-app>
+      <v-col cols="12" sm="12" md="12">
+        <v-card class="mx-auto text-center">
+          <v-card-titl>
+            <h4 class="headline fill-width">サークル編集</h4>
+          </v-card-titl>
+          <v-form ref="form" lazy-validation>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="12" md="12">
+                  <v-avatar max-width="40" max-height="40">
+                    <v-img v-if="photoURL" :src="photoURL" :lazy-src="photoURL">
+                      <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                          ></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                    <v-icon v-else large>mdi-account-outline</v-icon>
+                  </v-avatar>
+                </v-col>
+                <v-col cols="12" sm="12" md="12">
+                  <v-text-field
+                    prepend-icon="mdi-card-account-details-outline"
+                    label="サークル名"
+                    persistent-hint
+                    hint="サークル名を入力してください"
+                    :rules="[validRules.nameRules.required]"
+                    clearable
+                  />
+                </v-col>
+                <v-col cols="12" sm="12" md="12">
+                  <v-file-input
+                    ref="image"
+                    accept="image/*"
+                    show-size
+                    label="サークルアイコン"
+                    persistent-hint
+                    hint="ファイル選択画面が開きます"
+                    prepend-icon="mdi-image"
+                    @change="changeTeamImageFile"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeUpdateTeam">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="handleUpdateTeam">Save</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-app>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import FormValidation from '@/mixins/FormValidation.vue'
+
+export default {
+  mixins: [FormValidation],
+
+  props: {
+    updateTeamDialog: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters('modules/user/auth', ['photoURL'])
+  },
+  methods: {
+    changeTeamImageFile() {},
+    closeUpdateTeam() {
+      this.$emit('close-update-team')
+    },
+    ...mapActions('modules/team/team', ['updateTeam'])
+  }
+}
+</script>
