a84daae27d40439f882bb3bc9267372c212db899
Toru <Toru@Toru.local>
2020-11-19 17:35:30 +0900

    #409 change: メンバー登録入力フォームを適用させる

--- components/team/RegistrationMember.vue
+++ components/team/RegistrationMember.vue
@@ -1,56 +1,21 @@
 <template>
-  <FormDialog :form-dialog="registrationMemberDialog">
-    <template v-slot:dialog>
-      <FormView :title="'メンバー登録'">
-        <template v-slot:form>
-          <v-form ref="form" lazy-validation>
-            <v-row>
-              <v-col cols="12" sm="12" md="12">
-                <MemberName :name.sync="teamMember.name" :label="'名前'" />
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <RoleComboBox :items="teamRoles" :role.sync="teamMember.role" :label="'役割'" />
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <ImprovementRoleComboBox
-                  :items="improvementRoles"
-                  :improvementRole.sync="teamMember.improvementRole"
-                  :label="'改善担当'"
-                />
-              </v-col>
-            </v-row>
-          </v-form>
-
-          <SaveAndCloseButton
-            :close-button-title="'close'"
-            :save-button-title="'save'"
-            @close-button="closeRegistrationMember"
-            @save-button="handleRegistrationMember"
-          >
-            <template v-slot:save>
-              <v-icon left>mdi-account-plus</v-icon>
-            </template>
-          </SaveAndCloseButton>
-        </template>
-      </FormView>
-    </template>
-  </FormDialog>
+  <FormMember
+    :title="'メンバー登録'"
+    :team-member="teamMember"
+    :team-roles="teamRoles"
+    :member-dialog="memberDialog"
+    :improvement-roles="improvementRoles"
+    @close-member-dialog="closeRegistrationMember"
+    @save-member="handleRegistrationMember"
+  />
 </template>
 
 <script>
 import { mapActions } from 'vuex'
-import FormDialog from '@/components/commonParts/dialog/FormDialog'
-import SaveAndCloseButton from '@/components/commonParts/button/SaveAndCloseButton'
-import MemberName from '@/components/commonParts/team/input/MemberName'
-import RoleComboBox from '@/components/commonParts/team/input/RoleComboBox'
-import ImprovementRoleComboBox from '@/components/commonParts/team/input/ImprovementRoleComboBox'
-
+import FormMember from '@/components/commonParts/team/form/FormMember'
 export default {
   components: {
-    FormDialog,
-    SaveAndCloseButton,
-    RoleComboBox,
-    ImprovementRoleComboBox
+    FormMember
   },
   props: {
     teamMember: {
@@ -65,7 +30,7 @@ export default {
       type: Array,
       required: true
     },
-    registrationMemberDialog: {
+    memberDialog: {
       type: Boolean,
       required: true
     }
@@ -73,10 +38,7 @@ export default {
   methods: {
     async handleRegistrationMember() {
       const teamMember = this.teamMember
-      if (!teamMember.name) {
-        this.$refs.form.validate()
-        return
-      }
+
       await this.registrationMember(teamMember)
       this.closeRegistrationMember()
     },
--- components/team/edit/UpdateMember.vue
+++ components/team/edit/UpdateMember.vue
@@ -1,86 +1,26 @@
 <template>
-  <FormDialog :form-dialog="updateMemberDialog">
-    <template v-slot:dialog>
-      <v-col cols="12" sm="12" md="12">
-        <v-card>
-          <v-card-title>
-            <span class="headline text--center">メンバー編集</span>
-          </v-card-title>
-          <v-form ref="form" lazy-validation>
-            <v-container>
-              <v-row>
-                <!-- タスク編集エリア -->
-                <v-col cols="12" sm="12" md="12">
-                  <v-text-field
-                    ref="editMemberName"
-                    v-model="editMember.name"
-                    label="名前"
-                    required
-                    clearable
-                    dense
-                    prepend-inner-icon="mdi-card-account-details-outline"
-                    :rules="[validRules.nameRules.required]"
-                  />
-                </v-col>
-                <!-- 日付編集エリア -->
-                <v-col cols="12">
-                  <v-combobox
-                    v-model="editMember.role"
-                    :items="teamRoles"
-                    label="サークル役割"
-                    clearable
-                    multiple
-                    small-chips
-                    persistent-hint
-                    dense
-                    prepend-inner-icon="mdi-briefcase-account-outline"
-                    hint="文字入力の場合はEnterキーを押してください"
-                    @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
-                  />
-                </v-col>
-                <!-- 詳細編集エリア -->
-                <v-col cols="12">
-                  <v-combobox
-                    v-model="editMember.improvementRole"
-                    :items="improvementRoles"
-                    label="改善事例担当"
-                    clearable
-                    persistent-hint
-                    hint="文字入力の場合はEnterキーを押してください"
-                    multiple
-                    small-chips
-                    prepend-inner-icon="mdi-briefcase-outline"
-                    @click:clear="$nextTick(() => (searchedCategoryKeyword = null))"
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeUpdateMember">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="handleUpdateMember">Save</v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card>
-      </v-col>
-    </template>
-  </FormDialog>
+  <FormMember
+    :title="'メンバー編集'"
+    :team-member="editMember"
+    :team-roles="teamRoles"
+    :member-dialog="updateMemberDialog"
+    :improvement-roles="improvementRoles"
+    @close-member-dialog="closeUpdateMember"
+    @save-member="handleUpdateMember"
+  />
 </template>
 <script>
 import { mapActions } from 'vuex'
-import FormDialog from '@/components/commonParts/dialog/FormDialog'
-
-import FormValidation from '@/mixins/FormValidation.vue'
+import FormMember from '@/components/commonParts/team/form/FormMember'
 
 export default {
-  mixins: [FormValidation],
   components: {
-    FormDialog
+    FormMember
   },
   props: {
     editMember: {
       type: Object,
-      required: false
+      required: true
     },
     teamRoles: {
       type: Array,
@@ -99,10 +39,6 @@ export default {
   methods: {
     async handleUpdateMember() {
       const editMember = this.editMember
-      if (!editMember.name) {
-        this.$refs.form.validate()
-        return
-      }
       await this.updateMember(editMember)
       this.closeUpdateMember()
     },
