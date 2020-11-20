1fe6b2f83266a9cd816489502fdf708509f00fc4
Toru <Toru@Toru.local>
2020-11-21 03:21:18 +0900

    #446 Add: 画像にローディング表示をつけたコンポーネントを作成

---
+++ components/commonParts/v-img/LoadingImg.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-img :src="src" :lazy-src="src" aspect-ratio="1" class="grey lighten-2">
+    <template v-slot:placeholder>
+      <v-row class="fill-height ma-0" align="center" justify="center">
+        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+      </v-row>
+    </template>
+  </v-img>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
