0ae0fb446da7feb857996954b68d2fae84405fbc
Toru <Toru@Toru.local>
2020-11-28 07:46:13 +0900

    #549 Change:パスを変更

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,13 +3,14 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _1aa39070 = () => interopDefault(import('../pages/activityPlans.vue' /* webpackChunkName: "pages/activityPlans" */))
-const _14038586 = () => interopDefault(import('../pages/signIn.vue' /* webpackChunkName: "pages/signIn" */))
+const _fd12f094 = () => interopDefault(import('../pages/activityPlans.vue' /* webpackChunkName: "pages/activityPlans" */))
+const _cbaf6e92 = () => interopDefault(import('../pages/signIn.vue' /* webpackChunkName: "pages/signIn" */))
+const _455f8d12 = () => interopDefault(import('../pages/team.vue' /* webpackChunkName: "pages/team" */))
 
 // TODO: remove in Nuxt 3
-const emptyFn = () => {}
+const emptyFn = () => { }
 const originalPush = Router.prototype.push
-Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+Router.prototype.push = function push(location, onComplete = emptyFn, onAbort) {
   return originalPush.call(this, location, onComplete, onAbort)
 }
 
@@ -23,18 +24,22 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "/activityPlans",
-    component: _1aa39070,
+    path: "/",
+    component: _fd12f094,
     name: "activityPlans"
   }, {
     path: "/signIn",
-    component: _14038586,
+    component: _cbaf6e92,
     name: "signIn"
+  }, {
+    path: "/team",
+    component: _455f8d12,
+    name: "team"
   }],
 
   fallback: false
 }
 
-export function createRouter () {
+export function createRouter() {
   return new Router(routerOptions)
 }
