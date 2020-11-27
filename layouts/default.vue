65844a1afb73330d005c2f2c756955847b130731
Toru <Toru@Toru.local>
2020-11-28 07:33:15 +0900

    #535 Add: メインの背景色を追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app :style="{ background: $vuetify.theme.themes.light.mainBackground }">
+  <v-app :style="{ background: $vuetify.theme.themes.dark.mainBackground }">
     <Header />
     <transition name="page">
       <nuxt />
