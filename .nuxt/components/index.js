ff7276ccec1c82ba8b149996cea01a9c21d6ede1
Toru <Toru@Toru.local>
2020-11-28 13:18:28 +0900

    gitignore指定ファイル

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -25,7 +25,6 @@ export { default as UpdateEmail } from '../../components/User/UpdateUserInfo/Upd
 export { default as UpdatePassword } from '../../components/User/UpdateUserInfo/UpdatePassword.vue'
 export { default as UpdateUserAvatar } from '../../components/User/UpdateUserInfo/UpdateUserAvatar.vue'
 export { default as UpdateUserName } from '../../components/User/UpdateUserInfo/UpdateUserName.vue'
-export { default as FormDialog } from '../../components/commonParts/dialog/FormDialog.vue'
 export { default as BlueDialogButton } from '../../components/commonParts/button/BlueDialogButton.vue'
 export { default as CloseButton } from '../../components/commonParts/button/CloseButton.vue'
 export { default as DeleteButton } from '../../components/commonParts/button/DeleteButton.vue'
@@ -35,10 +34,14 @@ export { default as LoginButton } from '../../components/commonParts/button/Logi
 export { default as SaveAndCloseButton } from '../../components/commonParts/button/SaveAndCloseButton.vue'
 export { default as SaveButton } from '../../components/commonParts/button/SaveButton.vue'
 export { default as FormCardText } from '../../components/commonParts/card/FormCardText.vue'
+export { default as FormDialog } from '../../components/commonParts/dialog/FormDialog.vue'
 export { default as FormView } from '../../components/commonParts/form/FormView.vue'
 export { default as InputFile } from '../../components/commonParts/form/InputFile.vue'
 export { default as DoneSnackbar } from '../../components/commonParts/snackbar/DoneSnackbar.vue'
 export { default as LoadingImg } from '../../components/commonParts/v-img/LoadingImg.vue'
+export { default as EditTeamList } from '../../components/team/edit/EditTeamList.vue'
+export { default as UpdateMember } from '../../components/team/edit/UpdateMember.vue'
+export { default as UpdateTeam } from '../../components/team/edit/UpdateTeam.vue'
 export { default as FormCreateActivityPlan } from '../../components/commonParts/activityPlans/form/FormCreateActivityPlan.vue'
 export { default as CategoryCombobox } from '../../components/commonParts/activityPlans/input/CategoryCombobox.vue'
 export { default as DateForm } from '../../components/commonParts/activityPlans/input/DateForm.vue'
@@ -46,14 +49,11 @@ export { default as DetailForm } from '../../components/commonParts/activityPlan
 export { default as InChargeForm } from '../../components/commonParts/activityPlans/input/InChargeForm.vue'
 export { default as TextButton } from '../../components/commonParts/button/textButton/TextButton.vue'
 export { default as TextButtonXSmall } from '../../components/commonParts/button/textButton/TextButtonXSmall.vue'
-export { default as EditTeamList } from '../../components/team/edit/EditTeamList.vue'
-export { default as UpdateMember } from '../../components/team/edit/UpdateMember.vue'
-export { default as UpdateTeam } from '../../components/team/edit/UpdateTeam.vue'
+export { default as FormCreateTeam } from '../../components/commonParts/team/form/FormCreateTeam.vue'
+export { default as FormMember } from '../../components/commonParts/team/form/FormMember.vue'
 export { default as ImprovementRoleComboBox } from '../../components/commonParts/team/input/ImprovementRoleComboBox.vue'
 export { default as MemberName } from '../../components/commonParts/team/input/MemberName.vue'
 export { default as RoleComboBox } from '../../components/commonParts/team/input/RoleComboBox.vue'
-export { default as FormCreateTeam } from '../../components/commonParts/team/form/FormCreateTeam.vue'
-export { default as FormMember } from '../../components/commonParts/team/form/FormMember.vue'
 export { default as FormUserEmail } from '../../components/commonParts/user/form/FormUserEmail.vue'
 export { default as FormUserName } from '../../components/commonParts/user/form/FormUserName.vue'
 export { default as FormUserPassword } from '../../components/commonParts/user/form/FormUserPassword.vue'
@@ -85,7 +85,6 @@ export const LazyUpdateEmail = import('../../components/User/UpdateUserInfo/Upda
 export const LazyUpdatePassword = import('../../components/User/UpdateUserInfo/UpdatePassword.vue' /* webpackChunkName: "components/User/UpdateUserInfo/UpdatePassword" */).then(c => c.default || c)
 export const LazyUpdateUserAvatar = import('../../components/User/UpdateUserInfo/UpdateUserAvatar.vue' /* webpackChunkName: "components/User/UpdateUserInfo/UpdateUserAvatar" */).then(c => c.default || c)
 export const LazyUpdateUserName = import('../../components/User/UpdateUserInfo/UpdateUserName.vue' /* webpackChunkName: "components/User/UpdateUserInfo/UpdateUserName" */).then(c => c.default || c)
-export const LazyFormDialog = import('../../components/commonParts/dialog/FormDialog.vue' /* webpackChunkName: "components/commonParts/dialog/FormDialog" */).then(c => c.default || c)
 export const LazyBlueDialogButton = import('../../components/commonParts/button/BlueDialogButton.vue' /* webpackChunkName: "components/commonParts/button/BlueDialogButton" */).then(c => c.default || c)
 export const LazyCloseButton = import('../../components/commonParts/button/CloseButton.vue' /* webpackChunkName: "components/commonParts/button/CloseButton" */).then(c => c.default || c)
 export const LazyDeleteButton = import('../../components/commonParts/button/DeleteButton.vue' /* webpackChunkName: "components/commonParts/button/DeleteButton" */).then(c => c.default || c)
@@ -95,10 +94,14 @@ export const LazyLoginButton = import('../../components/commonParts/button/Login
 export const LazySaveAndCloseButton = import('../../components/commonParts/button/SaveAndCloseButton.vue' /* webpackChunkName: "components/commonParts/button/SaveAndCloseButton" */).then(c => c.default || c)
 export const LazySaveButton = import('../../components/commonParts/button/SaveButton.vue' /* webpackChunkName: "components/commonParts/button/SaveButton" */).then(c => c.default || c)
 export const LazyFormCardText = import('../../components/commonParts/card/FormCardText.vue' /* webpackChunkName: "components/commonParts/card/FormCardText" */).then(c => c.default || c)
+export const LazyFormDialog = import('../../components/commonParts/dialog/FormDialog.vue' /* webpackChunkName: "components/commonParts/dialog/FormDialog" */).then(c => c.default || c)
 export const LazyFormView = import('../../components/commonParts/form/FormView.vue' /* webpackChunkName: "components/commonParts/form/FormView" */).then(c => c.default || c)
 export const LazyInputFile = import('../../components/commonParts/form/InputFile.vue' /* webpackChunkName: "components/commonParts/form/InputFile" */).then(c => c.default || c)
 export const LazyDoneSnackbar = import('../../components/commonParts/snackbar/DoneSnackbar.vue' /* webpackChunkName: "components/commonParts/snackbar/DoneSnackbar" */).then(c => c.default || c)
 export const LazyLoadingImg = import('../../components/commonParts/v-img/LoadingImg.vue' /* webpackChunkName: "components/commonParts/v-img/LoadingImg" */).then(c => c.default || c)
+export const LazyEditTeamList = import('../../components/team/edit/EditTeamList.vue' /* webpackChunkName: "components/team/edit/EditTeamList" */).then(c => c.default || c)
+export const LazyUpdateMember = import('../../components/team/edit/UpdateMember.vue' /* webpackChunkName: "components/team/edit/UpdateMember" */).then(c => c.default || c)
+export const LazyUpdateTeam = import('../../components/team/edit/UpdateTeam.vue' /* webpackChunkName: "components/team/edit/UpdateTeam" */).then(c => c.default || c)
 export const LazyFormCreateActivityPlan = import('../../components/commonParts/activityPlans/form/FormCreateActivityPlan.vue' /* webpackChunkName: "components/commonParts/activityPlans/form/FormCreateActivityPlan" */).then(c => c.default || c)
 export const LazyCategoryCombobox = import('../../components/commonParts/activityPlans/input/CategoryCombobox.vue' /* webpackChunkName: "components/commonParts/activityPlans/input/CategoryCombobox" */).then(c => c.default || c)
 export const LazyDateForm = import('../../components/commonParts/activityPlans/input/DateForm.vue' /* webpackChunkName: "components/commonParts/activityPlans/input/DateForm" */).then(c => c.default || c)
@@ -106,14 +109,11 @@ export const LazyDetailForm = import('../../components/commonParts/activityPlans
 export const LazyInChargeForm = import('../../components/commonParts/activityPlans/input/InChargeForm.vue' /* webpackChunkName: "components/commonParts/activityPlans/input/InChargeForm" */).then(c => c.default || c)
 export const LazyTextButton = import('../../components/commonParts/button/textButton/TextButton.vue' /* webpackChunkName: "components/commonParts/button/textButton/TextButton" */).then(c => c.default || c)
 export const LazyTextButtonXSmall = import('../../components/commonParts/button/textButton/TextButtonXSmall.vue' /* webpackChunkName: "components/commonParts/button/textButton/TextButtonXSmall" */).then(c => c.default || c)
-export const LazyEditTeamList = import('../../components/team/edit/EditTeamList.vue' /* webpackChunkName: "components/team/edit/EditTeamList" */).then(c => c.default || c)
-export const LazyUpdateMember = import('../../components/team/edit/UpdateMember.vue' /* webpackChunkName: "components/team/edit/UpdateMember" */).then(c => c.default || c)
-export const LazyUpdateTeam = import('../../components/team/edit/UpdateTeam.vue' /* webpackChunkName: "components/team/edit/UpdateTeam" */).then(c => c.default || c)
+export const LazyFormCreateTeam = import('../../components/commonParts/team/form/FormCreateTeam.vue' /* webpackChunkName: "components/commonParts/team/form/FormCreateTeam" */).then(c => c.default || c)
+export const LazyFormMember = import('../../components/commonParts/team/form/FormMember.vue' /* webpackChunkName: "components/commonParts/team/form/FormMember" */).then(c => c.default || c)
 export const LazyImprovementRoleComboBox = import('../../components/commonParts/team/input/ImprovementRoleComboBox.vue' /* webpackChunkName: "components/commonParts/team/input/ImprovementRoleComboBox" */).then(c => c.default || c)
 export const LazyMemberName = import('../../components/commonParts/team/input/MemberName.vue' /* webpackChunkName: "components/commonParts/team/input/MemberName" */).then(c => c.default || c)
 export const LazyRoleComboBox = import('../../components/commonParts/team/input/RoleComboBox.vue' /* webpackChunkName: "components/commonParts/team/input/RoleComboBox" */).then(c => c.default || c)
-export const LazyFormCreateTeam = import('../../components/commonParts/team/form/FormCreateTeam.vue' /* webpackChunkName: "components/commonParts/team/form/FormCreateTeam" */).then(c => c.default || c)
-export const LazyFormMember = import('../../components/commonParts/team/form/FormMember.vue' /* webpackChunkName: "components/commonParts/team/form/FormMember" */).then(c => c.default || c)
 export const LazyFormUserEmail = import('../../components/commonParts/user/form/FormUserEmail.vue' /* webpackChunkName: "components/commonParts/user/form/FormUserEmail" */).then(c => c.default || c)
 export const LazyFormUserName = import('../../components/commonParts/user/form/FormUserName.vue' /* webpackChunkName: "components/commonParts/user/form/FormUserName" */).then(c => c.default || c)
 export const LazyFormUserPassword = import('../../components/commonParts/user/form/FormUserPassword.vue' /* webpackChunkName: "components/commonParts/user/form/FormUserPassword" */).then(c => c.default || c)
