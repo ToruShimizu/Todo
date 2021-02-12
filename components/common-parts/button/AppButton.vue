8756d1982d2d9b5586c9a10d17daeef478bdf47c
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-13 00:39:29 +0900

    Fix: #594 AppButtonコンポーネントを修正

--- components/common-parts/button/AppButton.vue
+++ components/common-parts/button/AppButton.vue
@@ -1,18 +1,67 @@
 <template>
-  <v-btn text @click="handleTextButton">
-    <v-icon>{{ icon }}</v-icon
-    >{{ title }}
+  <v-btn
+    class="app-btn font-weight-bold"
+    :class="{ 'white--text': !outlined, 'bg-white-btn ': outlined }"
+    :outlined="outlined"
+    :height="height"
+    :width="width"
+    :color="color"
+    :disabled="disabled"
+    :loading="loading"
+    :icon="icon"
+    :text="text"
+    depressed
+    @click="$emit('click')"
+  >
+    <slot v-if="!loading" />
   </v-btn>
 </template>
-
 <script>
 export default {
-  props: { icon: String, title: String },
-  methods: {
-    handleTextButton() {
-      this.$emit('handle-text-button')
+  name: 'AppBtn',
+
+  props: {
+    height: {
+      type: String,
+      default: '36px'
+    },
+    width: {
+      type: String,
+      default: '95px'
+    },
+    color: {
+      type: String,
+      default: 'accent'
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: Boolean,
+      default: false
     }
   }
 }
 </script>
-  
+
+<style scoped>
+.app-btn {
+  border-radius: 10px;
+}
+.bg-white-btn {
+  background-color: white;
+}
+</style>
