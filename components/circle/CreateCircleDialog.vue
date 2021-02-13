ddddbc534c16f769214376cb3147dcfab5eb9ab6
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-14 00:48:34 +0900

    Rename: #592 CreateCircleDialogに変更

--- components/circle/CreateCircleDialog.vue
+++ components/circle/CreateCircleDialog.vue
@@ -1,11 +1,16 @@
 <template>
-  <AppDialog :is-opened="isOpened" @close="$emit('close', false)" title="サークル新規作成">
+  <AppDialog
+    :is-opened="isOpened"
+    class="create-circle-dialog"
+    @close="$emit('close', false)"
+    title="サークル新規作成"
+  >
     <v-form v-model="isValid" ref="form" lazy-validation>
       <!-- チーム画像表示 -->
       <v-row class="mx-2">
-        <v-col cols="12" sm="12" md="12" class="text-center">
+        <v-col cols="12" class="text-center">
           <v-avatar size="200px">
-            <LoadingImg v-if="teamPhotoURL" :src="teamPhotoURL" width="200" />
+            <LoadingImg v-if="circlePhotoURL" :src="circlePhotoURL" width="200" />
             <v-icon v-else large>mdi-account-outline</v-icon>
           </v-avatar>
         </v-col>
@@ -13,9 +18,9 @@
 
       <!-- サークル名入力 -->
       <v-row class="mx-2">
-        <v-col cols="12" sm="12" md="12">
+        <v-col cols="12">
           <v-text-field
-            v-model="createTeamInput.name"
+            v-model="createCircleInput.name"
             prepend-icon="mdi-card-account-details-outline"
             label="サークル名"
             persistent-hint
@@ -28,14 +33,19 @@
 
       <!-- サークル画像選択 -->
       <v-row class="mx-2">
-        <v-col cols="12" sm="12" md="12">
-          <InputFile :imageFile="createTeamInput.imageFile" @change-image-file="changeImageFile" />
+        <v-col cols="12">
+          <InputFile
+            :imageFile="createCircleInput.imageFile"
+            @change-image-file="changeImageFile"
+          />
         </v-col>
       </v-row>
     </v-form>
     <!-- 保存、閉じるボタン -->
     <template slot="buttons">
-      <AppButton :disabled="isValid" :loading="isRunning" @click="createTeam">保存する </AppButton>
+      <AppButton :disabled="isValid" :loading="isRunning" @click="runCreateCircle"
+        >保存する
+      </AppButton>
       <AppButton color="success" outlined :disabled="isRunning" @click="$emit('close', false)"
         >キャンセル
       </AppButton>
@@ -45,6 +55,7 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 export default {
+  name: 'CreateCircleDialog',
   model: {
     prop: 'isOpened',
     event: 'close'
@@ -57,7 +68,7 @@ export default {
   },
   data() {
     return {
-      createTeamInput: {
+      createCircleInput: {
         name: '',
         imageFile: null,
         fileName: '',
@@ -69,24 +80,24 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('modules/team/team', ['teamPhotoURL'])
+    ...mapGetters('modules/circle', ['circlePhotoURL'])
   },
   methods: {
     // 画像ファイル変換
     changeImageFile(file) {
-      this.team.imageFile = file
+      this.ircle.imageFile = file
     },
     // サークル作成
-    async createTeam() {
-      if (this.createTeamInput.imageFile) {
-        await this.uploadTeamImageFile(this.createTeamInput.imageFile)
+    async runCreateCircle() {
+      if (this.createCircleInput.imageFile) {
+        await this.uploadCircleImageFile(this.createCircleInput.imageFile)
       } else {
-        await this.registrationTeam(this.createTeamInput)
+        await this.createCircle(this.createCircleInput)
       }
       this.$emit('close', false)
     },
 
-    ...mapActions('modules/team/team', ['registrationTeam', 'uploadTeamImageFile'])
+    ...mapActions('modules/circle', ['createCircle', 'uploadCircleImageFile'])
   }
 }
 </script>
