f395cb0a71df243b8f99418a69f1c360411effdd
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-14 11:27:51 +0900

    Fix: EsLintとprettierの設定を修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,32 +7,12 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'standard',
-    'prettier',
-    'prettier/vue',
-    'eslint:recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['@nuxtjs', 'standard', 'prettier', 'prettier/vue', 'eslint:recommended', 'plugin:prettier/recommended'],
   plugins: [],
   // add your custom rules here
   rules: {
-    'comma-dangle': ['error', 'only-multiline'],
-    'vue/max-attributes-per-line': 'off',
-    semi: [2, 'never'],
-    'no-duplicate-imports': 0,
-    'import/no-duplicates': 2,
-
-    // "prettier.semi":false,
-    'prettier/prettier': ['error', { semi: false }],
-    'dot-notation': [2, { allowPattern: '^[a-z]+(_[a-z]+)+$' }],
     'no-console': 'off',
-
-    // 文字列はシングルクオートのみ
-    quotes: [2, 'single'],
-    singleQuote: true,
-    trailingComma: true,
-    printWidth: 100
+    'vue/max-attributes-per-line': 'off',
+    'dot-notation': 'off'
   }
 }
